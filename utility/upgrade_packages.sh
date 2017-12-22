e123c1159fc2e7247ac2c8a9be867243a27544d6
rasmus <rasmus@peacegeeks.org>
2017-12-22 11:13:34 -0800

    Issue #158 Break script up into functions

--- utility/upgrade_packages.sh
+++ utility/upgrade_packages.sh
@@ -2,39 +2,71 @@
 
 ENVIRONMENT='venv-upgrade'
 
-if [[ $1 = 'local' ]]
-then
-    REQUIREMENTS='requirements/local.txt'
-    CONFIG='config.settings.local'
+main() {
+    handle_command_line_argument $1
+    upgrade_requirement_files
+    install_packages_in_clean_environment
+    run_tests
+    print_summary_message_to_standard_out
+}
 
-elif [[ $1 = 'test' ]]
-then
-    REQUIREMENTS='requirements/test.txt'
-    CONFIG='config.settings.test'
+handle_command_line_argument() {
+    if [[ $1 = 'local' ]]
+    then
+        REQUIREMENTS='requirements/local.txt'
+        CONFIG='config.settings.local'
 
-elif [[ $1 = 'production' ]]
-then
-    REQUIREMENTS='requirements/production.txt'
-    CONFIG='config.settings.production'
+    elif [[ $1 = 'test' ]]
+    then
+        REQUIREMENTS='requirements/test.txt'
+        CONFIG='config.settings.test'
 
-else
-    echo "Specify local, test or production"
-    exit
-fi
+    elif [[ $1 = 'production' ]]
+    then
+        REQUIREMENTS='requirements/production.txt'
+        CONFIG='config.settings.production'
 
-echo "Interactive upgrade of packages in $REQUIREMENTS"
-pur -r $REQUIREMENTS -i
+    else
+        echo "Specify local, test or production"
+        exit
+    fi
+}
 
-echo "Installing requirements from $REQUIREMENTS in clean environment $ENVIRONMENT"
-rm -rf $ENVIRONMENT
-python3 -m venv $ENVIRONMENT
-source $ENVIRONMENT/bin/activate
-pip install -r $REQUIREMENTS
-export DJANGO_SETTINGS_MODULE=$CONFIG
-python manage.py test
+upgrade_requirement_files() {
+    print_message "Interactive upgrade of packages in $REQUIREMENTS"
+    pur -r $REQUIREMENTS -i
+}
 
-echo "Execute"
-echo "   source $ENVIRONMENT/bin/activate"
-echo "   export DJANGO_SETTINGS_MODULE=$CONFIG"
-echo "and then perform whatever manual testing is needed"
-echo "before checking in changes in requirements/"
+print_message() {
+    echo
+    echo $1
+    echo
+}
+
+install_packages_in_clean_environment()
+{
+    print_message "Installing requirements from $REQUIREMENTS in clean environment $ENVIRONMENT"
+    rm -rf $ENVIRONMENT
+    python3 -m venv $ENVIRONMENT
+    source $ENVIRONMENT/bin/activate
+    pip install -r $REQUIREMENTS
+}
+
+run_tests() {
+    export DJANGO_SETTINGS_MODULE=$CONFIG
+    python manage.py test
+}
+
+print_summary_message_to_standard_out() {
+    echo
+    echo "Execute"
+    echo
+    echo "   source $ENVIRONMENT/bin/activate"
+    echo "   export DJANGO_SETTINGS_MODULE=$CONFIG"
+    echo
+    echo "and perform whatever manual testing is needed"
+    echo "before checking in changes in requirements/"
+    echo
+}
+
+main $1
