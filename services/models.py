d81912957d3217b4cb6bba8e36bd4d27f7a3e9d3
rasmus <rasmus@peacegeeks.org>
2018-01-17 15:40:39 -0800

    Issue #140 Clean up service model

--- services/models.py
+++ services/models.py
@@ -9,17 +9,16 @@ class Service(ValidateOnSaveMixin, TranslatableModel):
     id = RequiredCharField(primary_key=True,
                            max_length=200,
                            validators=[validators.validate_slug])
+
     organization = models.ForeignKey(Organization,
                                      on_delete=models.CASCADE,
                                      related_name='services')
-    translations = TranslatedFields(
-        name=models.CharField(max_length=200),
-        description=models.TextField(blank=True, null=True)
-    )
-    taxonomy_terms = models.ManyToManyField(
-        TaxonomyTerm,
-        db_table='services_service_taxonomy_terms'
-    )
+
+    taxonomy_terms = models.ManyToManyField(TaxonomyTerm,
+                                            db_table='services_service_taxonomy_terms')
+
+    translations = TranslatedFields(name=models.CharField(max_length=200),
+                                    description=models.TextField(blank=True, null=True))
 
     def __str__(self):
         return self.name
