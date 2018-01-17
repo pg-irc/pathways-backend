b06537ac8eed58337615f7bc0f21d2bb24ba5739
rasmus <rasmus@peacegeeks.org>
2018-01-16 16:47:40 -0800

    Issue #140 Add error handling and complete taxonomic search logic

--- services/tests/test_search.py
+++ services/tests/test_search.py
@@ -13,16 +13,42 @@ class ServicesTaxonomicSearchTests(rest_test.APITestCase):
     def test_search_with_taxonomic_argument_returns_service(self):
         taxonomy_term = TaxonomyTermBuilder().build()
         taxonomy_term.save()
-
         service = ServiceBuilder(self.organization).with_taxonomy_terms([taxonomy_term]).build()
-        service_id = service.id
         service.save()
 
-        taxonomy_id = taxonomy_term.taxonomy_id
-        name = taxonomy_term.name
-        url = '/v1/services/?taxonomy_term={0}:{1}'.format(taxonomy_id, name)
+        url = '/v1/services/?taxonomy_term={0}:{1}'.format(taxonomy_term.taxonomy_id,
+                                                           taxonomy_term.name)
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 1)
-        self.assertEqual(response.json()[0]['id'], service_id)
+        self.assertEqual(response.json()[0]['id'], service.id)
+
+    def test_search_with_wrong_taxonomic_argument_returns_404(self):
+        taxonomy_term = TaxonomyTermBuilder().build()
+        taxonomy_term.save()
+        second_taxonomy_term = TaxonomyTermBuilder().build()
+        second_taxonomy_term.save()
+        ServiceBuilder(self.organization).with_taxonomy_terms([second_taxonomy_term]).build().save()
+
+        url = '/v1/services/?taxonomy_term={0}:{1}'.format(taxonomy_term.taxonomy_id,
+                                                           taxonomy_term.name)
+        response = self.client.get(url)
+
+        #TODO this should throw 404, need middleware for that
+        self.assertEqual(len(response.json()), 0)
+
+    def test_taxonomic_argument_with_wrong_separator_returns_400(self):
+        url_with_plus_instead_of_colon = '/v1/services/?taxonomy_term=foo+bar'
+        response = self.client.get(url_with_plus_instead_of_colon)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_taxonomic_argument_with_missing_taxonomy_id_returns_400(self):
+        url_with_missing_taxonomy_id = '/v1/services/?taxonomy_term=:bar'
+        response = self.client.get(url_with_missing_taxonomy_id)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_taxonomic_argument_with_missing_taxonomy_term_returns_400(self):
+        url_with_missing_taxonomy_term = '/v1/services/?taxonomy_term=foo:'
+        response = self.client.get(url_with_missing_taxonomy_term)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- services/viewsets.py
+++ services/viewsets.py
@@ -1,20 +1,39 @@
 from rest_framework import viewsets
+from django.core.exceptions import SuspiciousOperation
 from services import models, serializers
 
 # pylint: disable=too-many-ancestors
 class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
+
     def get_queryset(self):
+        query_params = self.request.query_params
         queryset = models.Service.objects.all()
-        taxonomy_term = self.request.query_params.get('taxonomy_term', None)
-        if taxonomy_term:
-            taxonomy, term = self.parse_taxonomy_term_argument(taxonomy_term)
-            queryset = (queryset.
-                            filter(taxonomy_terms__name=term).
-                            filter(taxonomy_terms__taxonomy_id=taxonomy))
+
+        queryset = self.add_taxonomy_filter_if_given(query_params, queryset)
+
         return queryset
 
-    def parse_taxonomy_term_argument(self, term):
-        return term.split(':')
+    def add_taxonomy_filter_if_given(self, query_params, queryset):
+        taxonomy_term = query_params.get('taxonomy_term', None)
+        if taxonomy_term:
+            queryset = self.add_taxonomy_filter(queryset, taxonomy_term)
+        return queryset
+
+    def add_taxonomy_filter(self, queryset, parameter):
+        taxonomy, term = self.parse_taxonomy_parameter(parameter)
+        return (queryset.filter(taxonomy_terms__name=term).
+                         filter(taxonomy_terms__taxonomy_id=taxonomy))
+
+    def parse_taxonomy_parameter(self, parameter):
+        if parameter.count(':') != 1:
+            self.raise_taxonomy_error()
+        taxonomy, term = parameter.split(':')
+        if taxonomy=='' or term=='':
+            self.raise_taxonomy_error()
+        return taxonomy, term
+
+    def raise_taxonomy_error(self):
+        raise SuspiciousOperation('Invalid argument to taxonomy_term')
 
     serializer_class = serializers.ServiceSerializer
 
