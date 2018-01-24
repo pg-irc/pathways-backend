778d00a719492964e9b35c50aef52fe76f75980f
rasmus <rasmus@peacegeeks.org>
2018-01-23 19:58:52 -0800

    Issue #168 Clean up tests

--- services/tests/test_api.py
+++ services/tests/test_api.py
@@ -17,21 +17,19 @@ class ServicesApiTests(rest_test.APITestCase):
         }
 
     def test_can_get_services(self):
-        ServiceBuilder(self.organization).with_id(a_string()).create()
-        ServiceBuilder(self.organization).with_id(a_string()).create()
+        ServiceBuilder(self.organization).create()
+        ServiceBuilder(self.organization).create()
         url = '/v1/services/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 2)
 
     def test_can_get_one_service(self):
-        service_id = a_string()
-        service = ServiceBuilder(self.organization).with_id(service_id).build()
-        service.save()
-        url = '/v1/services/{0}/'.format(service_id)
+        service = ServiceBuilder(self.organization).create()
+        url = '/v1/services/{0}/'.format(service.id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()['id'], service_id)
+        self.assertEqual(response.json()['id'], service.id)
 
     def test_cannot_post(self):
         url = '/v1/services/'
@@ -39,35 +37,31 @@ class ServicesApiTests(rest_test.APITestCase):
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_put(self):
-        service = ServiceBuilder(self.organization).build()
-        service.save()
+        service = ServiceBuilder(self.organization).create()
         url = '/v1/services/{0}/'.format(service.pk)
         response = self.client.put(url, self.data)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_delete(self):
-        service = ServiceBuilder(self.organization).build()
-        service.save()
+        service = ServiceBuilder(self.organization).create()
         url = '/v1/services/{0}/'.format(service.pk)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_can_get_services_for_organization(self):
-        ServiceBuilder(self.organization).with_id(a_string()).create()
-        ServiceBuilder(self.organization).with_id(a_string()).create()
+        ServiceBuilder(self.organization).create()
+        ServiceBuilder(self.organization).create()
         url = '/v1/organizations/{0}/services/'.format(self.organization_id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 2)
 
     def test_can_get_one_service_for_organization(self):
-        description = a_string()
-        service = ServiceBuilder(self.organization).with_description(description).build()
-        service.save()
-        url = '/v1/organizations/{0}/services/{1}/'.format(self.organization_id, service.pk)
+        service = ServiceBuilder(self.organization).create()
+        url = '/v1/organizations/{0}/services/{1}/'.format(self.organization_id, service.id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()['description'], description)
+        self.assertEqual(response.json()['description'], service.description)
 
     def test_cannot_post_to_organization(self):
         url = '/v1/organizations/{0}/services/'.format(self.organization_id)
@@ -75,15 +69,13 @@ class ServicesApiTests(rest_test.APITestCase):
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_put_to_organization(self):
-        service = ServiceBuilder(self.organization).build()
-        service.save()
+        service = ServiceBuilder(self.organization).create()
         url = '/v1/organizations/{0}/services/{1}/'.format(self.organization_id, service.pk)
         response = self.client.put(url, self.data)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_delete_on_organization(self):
-        service = ServiceBuilder(self.organization).build()
-        service.save()
+        service = ServiceBuilder(self.organization).create()
         url = '/v1/organizations/{0}/services/{1}/'.format(self.organization_id, service.pk)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
