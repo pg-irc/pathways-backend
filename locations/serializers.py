b5a93a50f7fd7f084abe1172af8e7d4f7195f592
Ben St John <ben@affinitybridge.com>
2018-01-26 16:04:23 -0800

    Issue #134 Add location address serializer

--- locations/serializers.py
+++ locations/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from locations import models
+from addresses.serializers import AddressSerializer
 
-class LocationSerializer(serializers.HyperlinkedModelSerializer):
+
+class LocationAddressSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+    class Meta:
+        model = models.LocationAddress
+        fields = ('address_type', 'address')
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    location_addresses = LocationAddressSerializer(many=True)
     class Meta:
         model = models.Location
-        fields = ('id', 'name', 'organization_id', 'latitude', 'longitude', 'description')
+        fields = ('id', 'name', 'organization_id', 'latitude',
+                  'longitude', 'location_addresses', 'description')
