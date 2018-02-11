27e6cecfad34c633b84ea909adf490b0286301d6
rasmus <rasmus@peacegeeks.org>
2018-02-11 09:48:29 -0800

    Issue #177 Pull documentation out into a function so it can be shared among two views

--- human_services/services/viewsets.py
+++ human_services/services/viewsets.py
@@ -4,15 +4,9 @@ import coreapi, coreschema
 from human_services.services import models, serializers
 from . import private
 
-class SearchParameters:
-    def __init__(self, query_parameters):
-        self.taxonomy_id, self.taxonomy_term = private.parse_taxonomy_parameter(query_parameters)
-        self.full_text_search_terms = private.parse_full_text_search_terms(query_parameters)
-        self.sort_by = private.parse_sorting(query_parameters)
-
-# pylint: disable=too-many-ancestors
-class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
-    schema = AutoSchema(manual_fields=[
+def get_auto_schema_for_service_endpoint():
+    # TODO this is for list only!!!
+    return AutoSchema(manual_fields=[
         coreapi.Field(
             'search',
             location='query',
@@ -23,7 +17,7 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
             'taxonomy_term',
             location='query',
             description='Filter result on taxonomic terms, TODO make this take an array of terms with implied logical AND among terms, TODO make this work for hierarchical taxonomies',
-            schema=coreschema.String()
+            schema=coreschema.String(pattern='\w+:\w+')
         ),
         coreapi.Field(
             'sort_by',
@@ -32,6 +26,17 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
             schema=coreschema.Array(items=coreschema.String())
         ),
     ])
+
+class SearchParameters:
+    def __init__(self, query_parameters):
+        self.taxonomy_id, self.taxonomy_term = private.parse_taxonomy_parameter(query_parameters)
+        self.full_text_search_terms = private.parse_full_text_search_terms(query_parameters)
+        self.sort_by = private.parse_sorting(query_parameters)
+
+# pylint: disable=too-many-ancestors
+class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
+    schema = get_auto_schema_for_service_endpoint()
+
     def get_queryset(self):
         query_parameters = self.request.query_params
         search_parameters = SearchParameters(query_parameters)
@@ -42,6 +47,8 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
 
 # pylint: disable=too-many-ancestors
 class ServiceViewSetUnderOrganizations(viewsets.ReadOnlyModelViewSet):
+    schema = get_auto_schema_for_service_endpoint()
+
     def get_queryset(self):
         organization_id = self.kwargs['organization_id']
         return models.Service.objects.filter(organization=organization_id)
