5addf544d856d369b6f9066f4302a843a58851a3
rasmus <rasmus@peacegeeks.org>
2018-02-16 08:32:29 -0800

    Issue #177 Add more detail on the documentation for the /services end point

--- human_services/services/documentation.py
+++ human_services/services/documentation.py
@@ -1,20 +1,31 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from human_services.services.serializers import ServiceSerializer
 
 def get_list_schema_decorator():
 
-    taxonomy_term_description = ('Filter result on taxonomic terms, specify one term of the form '
-                                 'taxonomy:term. Examples: "bc211-what:libraries", '
-                                 '"bc211-who:service-providers", "bc211-why:homelessness". '
-                                 'TODO make this take an array of terms with implied logical AND '
-                                 'among terms. TODO make this work for hierarchical taxonomies.')
+    operation_description = 'Get a list of services'
 
-    taxonomy_term_param = openapi.Parameter('taxonomy_term',
-                                            openapi.IN_QUERY,
-                                            description=taxonomy_term_description,
-                                            type=openapi.TYPE_STRING)
+    taxonomy_description = ('Filter result on taxonomic terms, specify one term of the form '
+                            'taxonomy:term. Examples: "bc211-what:libraries", '
+                            '"bc211-who:service-providers", "bc211-why:homelessness". '
+                            'TODO make this take an array of terms with implied logical AND '
+                            'among terms. TODO make this work for hierarchical taxonomies.')
 
-    operation_description = 'Return a list of services'
+    manual_parameters = [openapi.Parameter('taxonomy_term',
+                                           openapi.IN_QUERY,
+                                           description=taxonomy_description,
+                                           type=openapi.TYPE_STRING,
+                                           pattern=r'\w+:\w'),
+                        ]
+
+    responses = {
+                    200: openapi.Response('a service', ServiceSerializer),
+                    400: 'invalid taxonomy term format, invalid field for sorting',
+                    404: 'invalid page',
+                }
 
     return swagger_auto_schema(operation_description=operation_description,
-                               manual_parameters=[taxonomy_term_param])
+                               manual_parameters=manual_parameters,
+                               responses=responses,
+                               )
