e7b11d3bf8bececdcd12875c86da3fb2b49e0aa1
Ben St John <ben@affinitybridge.com>
2018-02-21 16:09:57 -0800

    Issue #170 Create service at location viewset and use service at location proximity filter as the filter backend

--- human_services/locations/viewsets.py
+++ human_services/locations/viewsets.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from human_services.locations import models, serializers
+from common.filters import ServiceAtLocationProximityFilter
 
 # pylint: disable=too-many-ancestors
 class LocationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Location.objects.all()
     serializer_class = serializers.LocationSerializer
+
 
 # pylint: disable=too-many-ancestors
 class LocationViewSetUnderOrganizations(viewsets.ReadOnlyModelViewSet):
@@ -13,3 +15,10 @@ class LocationViewSetUnderOrganizations(viewsets.ReadOnlyModelViewSet):
         return models.Location.objects.filter(organization=organization_id)
 
     serializer_class = serializers.LocationSerializer
+
+
+class ServiceAtLocationViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.ServiceAtLocation.objects.all()
+    search_fields = ('location__translations__name', 'service__translations__name')
+    serializer_class = serializers.ServiceAtLocationSerializer
+    filter_backends = (ServiceAtLocationProximityFilter,)
