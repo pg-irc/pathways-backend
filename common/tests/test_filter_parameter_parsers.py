9b7ab9e58bce6e0f94c611e0cc4fdb4aec45a044
Ben St John <ben@affinitybridge.com>
2018-03-05 13:19:00 -0800

    Issue #170 Parser tests should not check error values

--- common/tests/test_filter_parameter_parsers.py
+++ common/tests/test_filter_parameter_parsers.py
@@ -9,39 +9,39 @@ class ProximityParserTests(TestCase):
         self.assertEqual(proximity.longitude, 22.2222)
 
     def test_throws_when_proximity_has_less_than_two_values(self):
-        with self.assertRaisesRegex(ParseError, ProximityParser.errors['exactly_two_values']):
+        with self.assertRaises(ParseError):
             ProximityParser('1.1111')
 
     def test_throws_when_proximity_has_more_than_two_values(self):
-        with self.assertRaisesRegex(ParseError, ProximityParser.errors['exactly_two_values']):
+        with self.assertRaises(ParseError):
             ProximityParser('1.1111,22.2222,33.3333')
 
     def test_throws_when_proximity_uses_non_comma_separator(self):
-        with self.assertRaisesRegex(ParseError, ProximityParser.errors['exactly_two_values']):
+        with self.assertRaises(ParseError):
             ProximityParser('1.1111&22.2222&333.3333')
 
     def test_throws_when_first_proximity_value_cannot_be_parsed_to_latitude(self):
-        with self.assertRaisesRegex(ParseError, ProximityParser.errors['invalid_latitude_value_type']):
+        with self.assertRaises(ParseError):
             ProximityParser('foo,1.1111')
 
     def test_throws_when_second_proximity_value_cannot_be_parsed_to_longitude(self):
-        with self.assertRaisesRegex(ParseError, ProximityParser.errors['invalid_longitude_value_type']):
+        with self.assertRaises(ParseError):
             ProximityParser('1.1111,foo')
 
 
 class TaxonomyParserTests(TestCase):
     def test_throws_when_too_many_field_separators(self):
-        with self.assertRaisesRegex(ParseError, TaxonomyParser.errors['exactly_two_values']):
+        with self.assertRaises(ParseError):
             TaxonomyParser('foo.bar.baz')
 
     def test_throws_when_missing_field_separators(self):
-        with self.assertRaisesRegex(ParseError, TaxonomyParser.errors['exactly_two_values']):
+        with self.assertRaises(ParseError):
             TaxonomyParser('foobar')
 
     def test_throws_when_missing_taxonomy_id(self):
-        with self.assertRaisesRegex(ParseError, TaxonomyParser.errors['empty_taxonomy_id']):
+        with self.assertRaises(ParseError):
             TaxonomyParser('.bar')
 
     def test_throws_when_missing_taxonomy_term(self):
-        with self.assertRaisesRegex(ParseError, TaxonomyParser.errors['empty_taxonomy_term']):
+        with self.assertRaises(ParseError):
             TaxonomyParser('foo.')
