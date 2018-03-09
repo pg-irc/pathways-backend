4224f44c736eafc8ca59e1e8eb19ffef6c3be323
rasmus <rasmus@peacegeeks.org>
2018-03-08 17:02:03 -0800

    Issue #154 Move tests to the new services_at_location app

---
+++ human_services/services_at_location/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.core import exceptions
+from human_services.locations.tests.helpers import LocationBuilder
+from human_services.locations.models import ServiceAtLocation
+from human_services.organizations.tests.helpers import OrganizationBuilder
+from human_services.services.tests.helpers import ServiceBuilder
+
+
+def validate_save_and_reload(instance):
+    instance.save()
+    instance.refresh_from_db()
+    return instance
+
+class TestServiceAtLocationModel(TestCase):
+    def setUp(self):
+        self.organization = OrganizationBuilder().build()
+        self.organization.save()
+
+        self.service = ServiceBuilder(self.organization).build()
+        self.service.save()
+
+        self.location = LocationBuilder(self.organization).build()
+        self.location.save()
+
+    def test_has_service_field(self):
+        service_at_location = ServiceAtLocation(service=self.service, location=self.location)
+        service_location_from_db = validate_save_and_reload(service_at_location)
+        self.assertEqual(service_location_from_db.service, self.service)
+
+    def test_service_cannot_be_none(self):
+        service_at_location = ServiceAtLocation(service=None, location=self.location)
+        with self.assertRaises(exceptions.ValidationError):
+            service_at_location.full_clean()
+
+    def test_has_location_field(self):
+        service_at_location = ServiceAtLocation(service=self.service, location=self.location)
+        service_location_from_db = validate_save_and_reload(service_at_location)
+        self.assertEqual(service_location_from_db.location, self.location)
+
+    def test_location_cannot_be_none(self):
+        service_at_location = ServiceAtLocation(service=self.service, location=None)
+        with self.assertRaises(exceptions.ValidationError):
+            service_at_location.full_clean()
