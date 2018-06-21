83fc2271af5d97f333784c8d9ca5494f14ea0681
rasmus <rasmus@peacegeeks.org>
2018-06-21 07:19:07 -0700

    Issue pg-irc/pathways-frontend#114 Build structure for producing json output for a task

---
+++ newcomers_guide/data_export.py
@@ -0,0 +1,30 @@
+import json
+
+
+class TaskBuilder:
+    def __init__(self):
+        self.task = {}
+
+    def get_id(self):
+        return self.task['id']
+
+    def set_id(self, the_id):
+        self.task['id'] = the_id
+        return self
+
+    def set_title_in_locale(self, locale, title):
+        self.ensure_key_exist('title')
+        self.task['title'][locale] = title
+        return self
+
+    def ensure_key_exist(self, key):
+        if key not in self.task:
+            self.task[key] = {}
+
+    def set_description_in_locale(self, locale, description):
+        self.ensure_key_exist('description')
+        self.task['description'][locale] = description
+        return self
+
+    def to_json(self):
+        return json.dumps(self.task)
---
+++ newcomers_guide/tests/test_data_export.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from newcomers_guide import data_export
+
+
+class TaskBuilderTest(TestCase):
+    def setUp(self):
+        self.task = data_export.TaskBuilder()
+
+    def test_can_set_id(self):
+        self.task.set_id('xyz')
+        self.assertEqual(self.task.to_json(), '{"id": "xyz"}')
+
+    def test_can_set_title_in_a_locale(self):
+        self.task.set_title_in_locale('en', 'xyz')
+        self.assertEqual(self.task.to_json(), '{"title": {"en": "xyz"}}')
+
+    def test_can_set_title_in_multile_locales(self):
+        self.task.set_title_in_locale('en', 'xyz')
+        self.task.set_title_in_locale('fr', 'abc')
+        self.assertEqual(self.task.to_json(), '{"title": {"en": "xyz", "fr": "abc"}}')
+
+    def test_can_set_description_in_a_locale(self):
+        self.task.set_description_in_locale('en', 'xyz')
+        self.assertEqual(self.task.to_json(), '{"description": {"en": "xyz"}}')
+
+    def test_can_create_complete_task(self):
+        self.task.set_id('the task id').\
+            set_title_in_locale('en', 'the title in English').\
+            set_title_in_locale('fr', 'the title in French').\
+            set_description_in_locale('en', 'the description in English').\
+            set_description_in_locale('fr', 'the description in French')
+
+        expected = ('{'
+                    '"id": "the task id", '
+                    '"title": {"en": "the title in English", "fr": "the title in French"}, '
+                    '"description": {"en": "the description in English", '
+                    '"fr": "the description in French"}'
+                    '}')
+        self.assertEqual(self.task.to_json(), expected)
