840f4c21e30b0b8e5e7c02bffc7734f19defced9
rasmus <rasmus@peacegeeks.org>
2018-07-04 13:55:48 -0700

    Issue pg-irc/pathways-frontend#114 Add questionnaire taxonomies to the system taxonomy map

--- newcomers_guide/system_data.py
+++ newcomers_guide/system_data.py
@@ -15,5 +15,51 @@ def get_system_taxonomies():
             'legal': {'icon': 'gavel'},
             'driving': {'icon': 'car'},
             'helpForIndividualsAndFamilies': {'icon': 'account'}
+        },
+        'time_in_canada':
+        {
+            'not_yet_arrived': {},
+            'under_1_month': {},
+            'under_6_months': {},
+            'under_1_year': {},
+            'under_2_years': {},
+            'over_2_years': {},
+        },
+        'user': {
+            'alone': {},
+            'with_family': {}
+        },
+        'age': {
+            'under_13': {},
+            '13_to_18': {},
+            '18_to_64': {},
+            'over_65': {},
+        },
+        'immigrant_type': {
+            'refugee_claimant': {},
+            'temporary_resident': {},
+            'permanent_resident': {},
+            'unknown': {},
+        },
+        'refugee_claim_stage': {
+            'claim_at_border': {},
+            'not_started': {},
+            'claim_at_cic_office': {},
+            'hearing': {},
+            'positive_decision': {},
+            'negative_decision': {},
+        },
+        'english_level': {
+            'none': {},
+            'beginner': {},
+            'intermediate': {},
+            'fluent': {},
+        },
+        'group': {
+            'women': {},
+            'disability': {},
+            'lgbtq2': {},
+            'services_in_french': {},
+            'low_income': {},
         }
     }
