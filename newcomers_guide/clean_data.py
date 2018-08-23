e5545a91f002d84ec27a8812a0109c4cdb3566b6
rasmus <rasmus@peacegeeks.org>
2018-08-23 15:37:08 -0700

    Issue #226 Break up a function

--- newcomers_guide/clean_data.py
+++ newcomers_guide/clean_data.py
@@ -5,8 +5,9 @@ def clean_up_newlines(text):
     text = remove_carriage_returns(text)
     text = remove_whitespace_between_newlines(text)
     text = remove_whitespace_before_newline(text)
-    text = protect_newlines_around_indentend_lines(text)
-    text = protect_newlines_around_markup(text)
+    text = protect_newlines_around_indented_lines(text)
+    text = protect_newlines_around_headings(text)
+    text = protect_newlines_around_bullet_list_items(text)
     text = protect_newlines_around_numbered_list_items(text)
     text = protect_multiple_newlines(text)
     text = replace_newlines_with_space(text)
@@ -36,7 +37,7 @@ def protect_multiple_newlines(text):
     return re.sub(two_newlines, r'NEWLINE_MARKER', text)
 
 
-def protect_newlines_around_indentend_lines(text):
+def protect_newlines_around_indented_lines(text):
     at_text_start = r'^([\t ][^\n]+)\n'
     text = re.sub(at_text_start, r'\1NEWLINE_MARKER', text)
 
@@ -47,14 +48,25 @@ def protect_newlines_around_indentend_lines(text):
     return re.sub(at_line_end, r'\1NEWLINE_MARKER', text)
 
 
-def protect_newlines_around_markup(text):
-    at_text_start = r'^([#\*\+\-][^\n]+)\n'
+def protect_newlines_around_headings(text):
+    at_text_start = r'^(#[^\n]+)\n'
     text = re.sub(at_text_start, r'\1NEWLINE_MARKER', text)
 
-    at_line_start = r'\n([#\*\+\-][^\n])'
+    at_line_start = r'\n(#[^\n])'
     text = re.sub(at_line_start, r'NEWLINE_MARKER\1', text)
 
-    at_line_end = r'(NEWLINE_MARKER[#\*\+\-][^\n]+)\n'
+    at_line_end = r'(NEWLINE_MARKER#[^\n]+)\n'
+    return re.sub(at_line_end, r'\1NEWLINE_MARKER', text)
+
+
+def protect_newlines_around_bullet_list_items(text):
+    at_text_start = r'^([\*\+\-][^\n]+)\n'
+    text = re.sub(at_text_start, r'\1NEWLINE_MARKER', text)
+
+    at_line_start = r'\n([\*\+\-][^\n])'
+    text = re.sub(at_line_start, r'NEWLINE_MARKER\1', text)
+
+    at_line_end = r'(NEWLINE_MARKER[\*\+\-][^\n]+)\n'
     return re.sub(at_line_end, r'\1NEWLINE_MARKER', text)
 
 
