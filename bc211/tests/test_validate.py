cb48fd58495054031945d8c40b4e3cb8f9089bd4
Ben St John <ben@affinitybridge.com>
2018-09-06 11:45:00 -0700

    Issue #231 Clean up

--- bc211/tests/test_validate.py
+++ bc211/tests/test_validate.py
@@ -13,19 +13,19 @@ class TestOptionalListOfObjects(unittest.TestCase):
 
     def test_throws_on_wrong_type_for_list_item_value(self):
         a_class = str
-        a_field = 'a_field'
+        a_field = a_string()
         a_value = a_list_of_integers()
         with self.assertRaises(exceptions.InvalidTypeXmlParseException):
             validate.optional_list_of_objects(a_class, a_field, {a_field: a_value})
 
     def test_returns_expected_value_for_empty_list(self):
         a_class = str
-        a_field = 'a_field'
+        a_field = a_string()
         a_value = []
         self.assertEqual(validate.optional_list_of_objects(a_class, a_field, {a_field: a_value}), a_value)
 
     def test_returns_expected_value_for_list_of_specified_class(self):
         a_class = str
-        a_field = 'a_field'
+        a_field = a_string()
         a_value = a_list_of_strings()
         self.assertEqual(validate.optional_list_of_objects(a_class, a_field, {a_field: a_value}), a_value)
