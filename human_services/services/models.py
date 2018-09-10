2d229d261dd5379df44f9239d0e6c2ba0d94517b
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-09-10 10:43:56 -0700

    Issue #228 Add on_delete triggers on foreign keys, as required by django 2.1

--- human_services/services/models.py
+++ human_services/services/models.py
@@ -5,12 +5,13 @@ from parler.models import TranslatableModel, TranslatedFields
 from human_services.organizations.models import Organization
 from taxonomies.models import TaxonomyTerm
 
+
 class Service(ValidateOnSaveMixin, TranslatableModel):
     id = RequiredCharField(primary_key=True,
                            max_length=200,
                            validators=[validators.validate_slug])
     organization = models.ForeignKey(Organization,
-                                     on_delete=models.CASCADE,
+                                     on_delete=models.PROTECT,
                                      related_name='services')
     taxonomy_terms = models.ManyToManyField(TaxonomyTerm,
                                             db_table='services_service_taxonomy_terms')
