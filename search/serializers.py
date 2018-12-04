444db9a838d196305568900e89d8884e92aa7a21
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-12-04 13:38:49 -0800

    Issue pg-irc/pathways-frontend#315 Set fields on REST response

--- search/serializers.py
+++ search/serializers.py
@@ -2,8 +2,17 @@ from rest_framework import serializers
 from search import models
 
 
-class RelatedTaskSerializer(serializers.HyperlinkedModelSerializer):
+class RelatedTaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.TaskSimilarityScore
-        fields = ('id', 'first_task_id', 'second_task_id', 'similarity_score')
+        fields = ('id', 'first_task_id', 'second_task_id', 'name', 'description', 'similarity_score')
+
+    name = serializers.SerializerMethodField('related_task_name')
+    description = serializers.SerializerMethodField('related_task_description')
+
+    def related_task_name(self, record):
+        return record.second_task.name
+
+    def related_task_description(self, record):
+        return record.second_task.description
--- search/tests/test_api.py
+++ search/tests/test_api.py
@@ -1,3 +1,4 @@
+from django.utils import translation
 from rest_framework import test as rest_test
 from rest_framework import status
 from common.testhelpers.random_test_values import a_string, a_float
@@ -6,22 +7,76 @@ from search.models import Task, TaskSimilarityScore
 
 class TaskApiTests(rest_test.APITestCase):
     def setUp(self):
+        translation.activate('en')
         self.first_task_id = a_string()
+
         self.second_task_id = a_string()
         self.second_task_name = a_string()
+        self.second_task_description = a_string()
+
         self.first_task = Task(id=self.first_task_id, name=a_string(), description=a_string())
         self.first_task.save()
-        self.second_task = Task(id=self.second_task_id, name=self.second_task_name, description=a_string())
+
+        self.second_task = Task(id=self.second_task_id, name=self.second_task_name,
+                                description=self.second_task_description)
         self.second_task.save()
+
         self.similarity_score = a_float()
         TaskSimilarityScore(first_task=self.first_task, second_task=self.second_task,
                             similarity_score=self.similarity_score).save()
 
-    def test_can_get_related_tasks_to_a_given_task(self):
+    def test_can_get_response(self):
         url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_the_task_id(self):
+        url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
+        response = self.client.get(url)
         self.assertEqual(len(response.json()), 1)
         self.assertEqual(response.json()[0]['first_task_id'], self.first_task_id)
+
+    def test_can_get_related_task_id(self):
+        url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
+        response = self.client.get(url)
+        self.assertEqual(len(response.json()), 1)
         self.assertEqual(response.json()[0]['second_task_id'], self.second_task_id)
+
+    def test_can_get_related_task_name(self):
+        url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
+        response = self.client.get(url)
+        self.assertEqual(response.json()[0]['name'], self.second_task_name)
+
+    def test_can_get_related_task_description(self):
+        url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
+        response = self.client.get(url)
+        self.assertEqual(response.json()[0]['description'], self.second_task_description)
+
+    def test_can_get_similarity_score(self):
+        url = '/v1/tasks/{}/related_tasks/'.format(self.first_task_id)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertAlmostEqual(response.json()[0]['similarity_score'], self.similarity_score)
+
+    def test_can_get_multiple_responses(self):
+
+        first_task_id = a_string()
+        second_task_id = a_string()
+        third_task_id = a_string()
+
+        first_task = Task(id=first_task_id, name=a_string(), description=a_string())
+        second_task = Task(id=second_task_id, name=a_string(), description=a_string())
+        third_task = Task(id=third_task_id, name=a_string(), description=a_string())
+
+        first_task.save()
+        second_task.save()
+        third_task.save()
+
+        TaskSimilarityScore(first_task=first_task, second_task=second_task,
+                            similarity_score=a_float()).save()
+        TaskSimilarityScore(first_task=first_task, second_task=third_task,
+                            similarity_score=a_float()).save()
+
+        url = '/v1/tasks/{}/related_tasks/'.format(first_task_id)
+        response = self.client.get(url)
+        self.assertEqual(len(response.json()), 2)
