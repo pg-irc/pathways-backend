4a46b60433b855f368e835e6164dfe93808d4650
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-02 13:41:01 -0800

    Issue #287 Add many to many relationship from task (topic) to taxonomy term

--- search/models.py
+++ search/models.py
@@ -3,12 +3,15 @@ from human_services.services.models import Service
 from django.core import validators
 from django.db import models
 from parler.models import TranslatableModel, TranslatedFields
+from taxonomies.models import TaxonomyTerm
 
 
 class Task(ValidateOnSaveMixin, TranslatableModel):
     id = RequiredCharField(primary_key=True,
                            max_length=200,
                            validators=[validators.validate_slug])
+    taxonomy_terms = models.ManyToManyField(TaxonomyTerm,
+                                            db_table='topic_taxonomy_terms')
     translations = TranslatedFields(
         name=models.CharField(blank=False, max_length=200),
         description=models.TextField(blank=True)
