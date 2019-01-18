118de97a7f4a4c101cce14089b339258934bb273
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-18 13:56:20 -0800

    Issue #297 Reduce the number of database quesries issued for a request to services_at_location

--- human_services/services_at_location/viewsets.py
+++ human_services/services_at_location/viewsets.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from django.utils.decorators import method_decorator
 from human_services.locations import models
 from human_services.services_at_location import documentation, serializers
-# TODO move common.filters to human_services.filters, 
+# TODO move common.filters to human_services.filters,
 # LocationIdFilter and similar should be with the location code
 from common.filters import (ProximityFilter, ProximityCutoffFilter, SearchFilter, LocationIdFilter,
                             ServiceIdFilter, TaxonomyFilter, ServiceSimilarityFilter)
@@ -11,7 +11,13 @@ from common.filters import (ProximityFilter, ProximityCutoffFilter, SearchFilter
 # pylint: disable=too-many-ancestors
 @method_decorator(name='list', decorator=documentation.get_service_at_location_list_schema())
 class ServiceAtLocationViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.ServiceAtLocation.objects.all()
+    queryset = (models.ServiceAtLocation.objects.
+                select_related('service').
+                select_related('location').
+                prefetch_related('service__translations').
+                prefetch_related('location__translations').
+                prefetch_related('location__location_addresses__address').
+                prefetch_related('location__phone_numbers'))
     serializer_class = serializers.ServiceAtLocationSerializer
     search_fields = ('location__translations__name', 'location__translations__description',
                      'service__translations__name', 'service__translations__description')
