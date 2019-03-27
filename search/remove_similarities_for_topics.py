a52c82590643e0ad8a5e09e3456b6091730e9152
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-03-27 10:44:41 -0700

    Issue #336 Remove topic-service similarity scores for given topics

---
+++ search/remove_similarities_for_topics.py
@@ -0,0 +1,12 @@
+import logging
+from search.models import TaskServiceSimilarityScore
+
+LOGGER = logging.getLogger('remove_similarities_for_topics')
+
+
+def remove_similarities_for_topics(list_of_task_ids):
+    for task_id in list_of_task_ids:
+        result = TaskServiceSimilarityScore.objects.filter(task_id=task_id).delete()
+        number_of_records_deleted = result[0]
+        if number_of_records_deleted == 0:
+            LOGGER.warn('%s: Invalid topic id', task_id)
