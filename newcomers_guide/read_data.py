6fea04d4c6c68ea565805d55c769af05e0d3e25d
Tomy Reyes <tomy@peacegeeks.org>
2019-05-14 13:55:49 -0700

    Issue #214 address code review comments

--- newcomers_guide/read_data.py
+++ newcomers_guide/read_data.py
@@ -13,8 +13,7 @@ def read_task_data(root_folder):
 
 
 def read_taxonomy_data(root_folder):
-    if not check_if_taxonomy_file_is_missing(root_folder):
-        raise Exception('There is a taxonomy file missing in the Newcomers Guide')
+    validate_taxonomy_data(root_folder)
     
     taxonomy_data = []
     for root, _, filenames in os.walk(root_folder, topdown=False):
@@ -48,16 +47,19 @@ def is_taxonomy_file(path):
     return path.endswith(sep + 'taxonomy.txt')
 
 
-def check_if_taxonomy_file_is_missing(root_folder):
-    paths = build_paths_to_taxonomy_files(root_folder)
-    taxonomy_files_exist = True
+def is_taxonomy_file_missing(root_folder):
+    paths = get_paths_to_topic_folders(root_folder)
     for path in paths:
-        for sub_path, _, filenames in os.walk(path, topdown=False):
-            exists = os.path.isfile(sub_path + '/taxonomy.txt')
+            exists = os.path.isfile(path + '/taxonomy.txt')
             if not exists:
-                taxonomy_files_exist = False
-                break
-    return taxonomy_files_exist
+                return True
+    return False
 
-def build_paths_to_taxonomy_files(root_folder):
-    return glob(root_folder + '*/'+ 'topics/' + '*/')   
+
+def get_paths_to_topic_folders(root_folder):
+    return glob(root_folder + '/*/topics/*/')  
+
+def validate_taxonomy_data(root_folder):
+    if is_taxonomy_file_missing(root_folder):
+        raise Exception('There is a taxonomy file missing in the Newcomers Guide')
+
