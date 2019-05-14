3310c17c25ba7c800a5015ecd5ecc25eafb80fcc
Tomy Reyes <tomy@peacegeeks.org>
2019-05-14 14:47:10 -0700

    Issue #345 address code review comments

---
+++ bc211/tests/test_version.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from rest_framework import status
+from bc211 import version
+
+
+class TestLastUpdate(TestCase):
+
+    def test_get_version_from_api(self):
+        self.bc211_version_string = version.get_version_string()
+        url = '/bc211version/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content.decode("utf-8"), self.bc211_version_string)
--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ from human_services.services.viewsets import ServiceViewSet
 from search.viewsets import RelatedTasksViewSet, RelatedServicesViewSet
 from rest_framework import routers
 from config import documentation
-from bc211.last_update import LastUpdateView
+from bc211.version import Bc211VersionView
 
 
 def build_router():
@@ -48,7 +48,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     url(r'^version/$', VersionView.as_view(), name='version'),
-    url(r'^dbversion/$', LastUpdateView.as_view(), name='last_update'),
+    url(r'^bc211version/$', Bc211VersionView.as_view(), name='bc211_version'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
