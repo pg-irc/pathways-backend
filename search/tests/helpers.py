79c04a25010d049573c911996d040cc3731f91ba
Tomy Reyes <tomy@peacegeeks.org>
2019-05-21 11:29:06 -0700

    Issue #317 address PR comments

--- search/tests/helpers.py
+++ search/tests/helpers.py
@@ -2,18 +2,18 @@ from common.testhelpers.random_test_values import a_float
 
 
 def create_square_matrix_of_unique_floats(number):
-    scores = [[a_float() for i in range(number)] for j in range(number)]
-    
-    while is_there_a_duplicate_score_in_square_matrix(scores):
-        create_square_matrix_of_unique_floats(number)
-    return scores
+    while True:
+        scores = [[a_float() for i in range(number)] for j in range(number)]
+        if not is_a_duplicate_score_in_square_matrix(scores):
+            return scores
 
 
-def is_there_a_duplicate_score_in_square_matrix(scores):
+def is_a_duplicate_score_in_square_matrix(scores):
     float_dictionary = {}
 
     for i in scores:
         for j in i:
             if j in float_dictionary:
-                True          
+                return True      
+            float_dictionary[j] = 1    
     return False
