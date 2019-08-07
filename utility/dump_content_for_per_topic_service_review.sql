9dd6b85b4457baa3470289980da8253420370373
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-07 11:51:38 -0700

    Issue #414 Include all the topic ids for things to do right away in example command.

--- utility/dump_content_for_per_topic_service_review.sql
+++ utility/dump_content_for_per_topic_service_review.sql
@@ -1,9 +1,25 @@
 -- Pass topic ids on the command line as a list using -v, mind the single and double quotes!
 
--- psql -v task_ids="('abuse-and-violence','animal-abuse')" -d pathways_local -F $'\t' -A -f utility/dump_content_for_per_topic_service_review.sql > content.csv
+-- command for getting the recommended services for all the topics in Things to do right away
+
+-- psql -d pathways_local -F $'\t' -A -f utility/dump_content_for_per_topic_service_review.sql \
+-- -v task_ids="('contact-workers-at-your-local-settlement-agency',\
+-- 'give-your-canadian-address-to-immigration-refugees-and-citizenship-canada',\
+-- 'finding-a-place-to-rent',\
+-- 'know-your-rights-as-a-tenant',\
+-- 'apply-for-the-canada-child-benefit-for-children-under-18-years-old',\
+-- 'opening-a-bank-account',\
+-- 'get-your-bc-services-card',\
+-- 'register-for-the-medical-services-plan-msp',\
+-- 'the-interim-federal-health-program-refugees-and-refugee-claimants',\
+-- 'register-for-english-classes',\
+-- 'registering-your-child-in-a-public-school',\
+-- 'applying-for-a-driver_s-licence',\
+-- 'apply-for-a-social-insurance-number-sin')"> content.csv
 
 select distinct
 	taskSimilarity.task_id as topic_id,
+	to_char(taskSimilarity.similarity_score, '9.99') as score,
 	service.id as service_id,
 	'' as "exclude?",
 	'"' || organizationStrings.name || '"' as organization_name,
