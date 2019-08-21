b3f0748878d4f8c95cb796668cf5b6f230060181
Ian Lin <ian@peacegeeks.org>
2019-08-21 15:38:11 -0700

    Issue #426 Query task taxonomies instead of bc211 taxonomies

--- utility/dump_content_for_taxonomies.sql
+++ utility/dump_content_for_taxonomies.sql
@@ -1,17 +1,9 @@
--- psql -d pathways_local -F $',' -A -f utility/dump_content_for_recommendations_review.sql \ 
--- -v taxonomy_id='bc211-why' > content.csv
+--psql -d pathways_local -F $',' -A -f utility/dump_content_for_taxonomies.sql  > content.csv
+-- For use in situations when we need the task taxonomies and their frequency:
 
--- For use in situations when we need the taxonomies and their frequency:
-
-select distinct 
-    taxonomies_taxonomyterm.name, count(taxonomies_taxonomyterm.name)
-from 
-    services_service_translation 
-left outer join services_service_taxonomy_terms
-    on (services_service_translation.master_id = services_service_taxonomy_terms.service_id)
-inner join taxonomies_taxonomyterm
-    on (services_service_taxonomy_terms.taxonomyterm_id = taxonomies_taxonomyterm.id)
-where 
-    taxonomies_taxonomyterm.taxonomy_id = :taxonomy_id
-group by 
-    taxonomies_taxonomyterm.name;
+select topic_taxonomy_terms.task_id, taxonomies_taxonomyterm.taxonomy_id, taxonomies_taxonomyterm.name
+from
+    topic_taxonomy_terms,
+    taxonomies_taxonomyterm
+where
+    topic_taxonomy_terms.taxonomyterm_id = taxonomies_taxonomyterm.id;
