e62803e34cc243b4620d8752896514b34d98c1f3
Ian Lin <ian@peacegeeks.org>
2019-08-21 13:24:46 -0700

    Issue #426 Refactored query for services without taxonomy

--- utility/dump_content_for_services_without_taxonomy.sql
+++ utility/dump_content_for_services_without_taxonomy.sql
@@ -2,13 +2,28 @@
 
 -- For use in situations when we need to find :
 
-select distinct 
-    services_service_translation.master_id, services_service_translation.name
-from 
-    services_service_translation 
-left outer join services_service_taxonomy_terms
-    on (services_service_translation.master_id = services_service_taxonomy_terms.service_id)
-where 
-    services_service_taxonomy_terms.service_id is null
-order by 
-    services_service_translation.name;
+select distinct
+	service.id as service_id,
+    serviceStrings.name as service_name,
+    organizationStrings.name as organization_name,
+    address.address
+from
+	services_service as service
+    left outer join services_service_taxonomy_terms
+        on(service.id = services_service_taxonomy_terms.service_id),
+	services_service_translation as serviceStrings,
+    organizations_organization as organization,
+	organizations_organization_translation as organizationStrings,
+	locations_location as location,
+	locations_serviceatlocation as servicesAtLocation,
+	locations_locationaddress as locationAddress,
+	addresses_address as address
+where
+    service.id = serviceStrings.master_id and
+    services_service_taxonomy_terms.service_id is null and 
+    organization.id=organizationStrings.master_id and
+	organization.id=service.organization_id and
+	service.id=servicesAtLocation.service_id and
+	location.id=servicesAtLocation.location_id and
+	servicesAtLocation.location_id=locationAddress.location_id and
+	locationAddress.address_id=address.id;
