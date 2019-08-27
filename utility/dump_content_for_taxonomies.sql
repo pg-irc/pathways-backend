43e5a1ef2cd092f5581908ef8c7a7b5807073126
Ian Lin <ian@peacegeeks.org>
2019-08-27 16:33:57 -0700

    Issue #426 Remove trivial sql script

--- utility/dump_content_for_taxonomies.sql
+++
@@ -1,9 +0,0 @@
---psql -d pathways_local -F $',' -A -f utility/dump_content_for_taxonomies.sql  > content.csv
--- For use in situations when we need the task taxonomies and their frequency:
-
-select topic_taxonomy_terms.task_id, taxonomies_taxonomyterm.taxonomy_id, taxonomies_taxonomyterm.name
-from
-    topic_taxonomy_terms,
-    taxonomies_taxonomyterm
-where
-    topic_taxonomy_terms.taxonomyterm_id = taxonomies_taxonomyterm.id;
