b50cb13ff90a3b4200fc02c4ac8171430299861b
Ian Lin <ian@peacegeeks.org>
2019-08-27 16:40:12 -0700

    Issue #426 Replace tabs with 4 spaces

--- utility/dump_content_for_services_with_no_taxonomy_term.sql
+++ utility/dump_content_for_services_with_no_taxonomy_term.sql
@@ -2,27 +2,27 @@
 -- psql -d pathways_local -F $',' -A -f utility/dump_content_for_services_with_no_taxonomy_term.sql > content.csv
 
 select distinct
-	service.id as service_id,
+    service.id as service_id,
     serviceStrings.name as service_name,
     organizationStrings.name as organization_name,
     address.address
 from
-	services_service as service
+    services_service as service
     left outer join services_service_taxonomy_terms
         on(service.id = services_service_taxonomy_terms.service_id),
-	services_service_translation as serviceStrings,
+    services_service_translation as serviceStrings,
     organizations_organization as organization,
-	organizations_organization_translation as organizationStrings,
-	locations_location as location,
-	locations_serviceatlocation as servicesAtLocation,
-	locations_locationaddress as locationAddress,
-	addresses_address as address
+    organizations_organization_translation as organizationStrings,
+    locations_location as location,
+    locations_serviceatlocation as servicesAtLocation,
+    locations_locationaddress as locationAddress,
+    addresses_address as address
 where
     service.id = serviceStrings.master_id and
     services_service_taxonomy_terms.service_id is null and 
     organization.id=organizationStrings.master_id and
-	organization.id=service.organization_id and
-	service.id=servicesAtLocation.service_id and
-	location.id=servicesAtLocation.location_id and
-	servicesAtLocation.location_id=locationAddress.location_id and
-	locationAddress.address_id=address.id;
+    organization.id=service.organization_id and
+    service.id=servicesAtLocation.service_id and
+    location.id=servicesAtLocation.location_id and
+    servicesAtLocation.location_id=locationAddress.location_id and
+    locationAddress.address_id=address.id;
