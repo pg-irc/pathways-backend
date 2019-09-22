341720cc9cfbba99e7abd9443d09052f108a159b
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-22 11:35:28 -0700

    Issue pg-irc/pathways-frontend#800 Sort bad links by length descending to correctly replace with good links

--- utility/fix_broken_links_in_newcomers_guide.py
+++ utility/fix_broken_links_in_newcomers_guide.py
@@ -30,31 +30,62 @@ def read_csv_data_from_file(csv_path):
     return result
 
 
-fixed_links = read_csv_data_from_file('../content/Fix broken links in AA - urls.csv')
+fixed_links = sorted(
+    read_csv_data_from_file('../content/Fix broken links in AA - urls.csv'),
+    key=lambda x: len(x[0]),
+    reverse=True
+)
+
+
+def fix_link(content, bad_link, good_link):
+    found = content.find(bad_link) != -1
+    if found:
+        print('FOUND {}, repacing with {}'.format(bad_link, good_link))
+        content = content.replace(bad_link, good_link)
+        print('==========START====================================================')
+        print(content)
+        print('===========END===================================================')
+    else:
+        print('NOT FOUND {}'.format(bad_link))
+    return [found, content]
 
 
 def fix_links_in_file(path):
     dirty = False
     content = read_file_content(path)
+    print()
+    print(path)
     for line in fixed_links:
         bad_link = line[0].strip()
-
-        bad_link_with_http = bad_link.replace('https', 'http')
-        bad_link_with_https = bad_link.replace('http', 'https')
-        contains_bad_link = content.find(bad_link_with_http) or content.find(bad_link_with_https)
-
         good_link = line[2].strip()
-        if contains_bad_link and good_link != 'OK':
-            content = content.replace(bad_link_with_http, good_link)
-            content = content.replace(bad_link_with_https, good_link)
-            dirty = True
+
+        if bad_link == '' or good_link == '' or good_link == 'OK':
+            continue
+
+        bad_link_with_http = bad_link.replace('https:', 'http:')
+        bad_link_with_https = bad_link.replace('http:', 'https:')
+
+        found1, content = fix_link(content, bad_link_with_http, good_link)
+
+        if found1:
+            print('REPLACED {} with {}'.format(bad_link_with_http, good_link))
+
+        found2, content = fix_link(content, bad_link_with_https, good_link)
+
+        if found2:
+            print('REPLACED {} with {}'.format(bad_link_with_https, good_link))
+
+        dirty = dirty or found1 or found2
+
     if dirty:
-        print(path)
+        print('Writing {}'.format(path))
+        print()
         write_file_content(path, content)
 
 
 def main():
-    for root, _, filenames in os.walk('../content/NewcomersGuide/', topdown=False):
+    folder = '../content/NewcomersGuide/Chapter 9 - Employment and Business/topics/Maternity and parental leave/'
+    for root, _, filenames in os.walk(folder, topdown=False):
         for filename in filenames:
             path = os.path.join(root, filename)
             if is_topic_file(path):
@@ -62,3 +93,7 @@ def main():
 
 
 main()
+
+
+# -https://www2.gov.bc.ca/gov/content/employmentbusiness/employment-standards-advice/employment-standards/factsheets/leaves-andjury-duty
+# +https://www2.gov.bc.ca/gov/content/employment-business/employment-standards-advice/employment-standards/factsheets/leaves-andjury-duty
