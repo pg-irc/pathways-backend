239172262b3f7f2a3591620b264ae345f19698cc
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-23 09:40:35 -0700

    Issue pg-irc/pathways-frontend#800 Pass path to newcomers guide on the command line

--- utility/extract_all_urls.py
+++ utility/extract_all_urls.py
@@ -1,7 +1,8 @@
-# PYTHONPATH=. python ./utility/extract_all_urls.py | sort | uniq > urls.txt
+# PYTHONPATH=. python ./utility/extract_all_urls.py ../content/NewcomersGuide/ | sort | uniq > urls.txt
 
-import re
 import os
+import sys
+import re
 from newcomers_guide.read_data import is_topic_file, read_file_content
 from newcomers_guide.link_regular_expression import url_regular_expression
 
@@ -14,7 +15,7 @@ def extract_urls(path):
 
 
 def main():
-    folder = '../content/NewcomersGuide/'
+    folder = sys.argv[1]
     for root, _, filenames in os.walk(folder, topdown=False):
         for filename in filenames:
             path = os.path.join(root, filename)
