8f80ac289c06ddcbd794e4e9c95ebf0c4e38a943
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-08 18:05:30 -0800

    Issue #441 Can post new token

--- push_notifications/tests/test_api.py
+++ push_notifications/tests/test_api.py
@@ -20,3 +20,14 @@ class ServicesApiTests(rest_test.APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 2)
+
+    def test_post_can_create_a_token(self):
+        the_token = a_string()
+        body = {'id': the_token}
+        url = '/v1/push_notifications/tokens/'
+        response = self.client.post(url, body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.json()), 1)
+        data = PushNotificationToken.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].id, the_token)
--- push_notifications/view_sets.py
+++ push_notifications/view_sets.py
@@ -1,7 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from push_notifications import models, serializers
+from rest_framework.response import Response
 
 
 class TokenViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.PushNotificationToken.objects.all()
     serializer_class = serializers.TokenSerializer
+
+    def create(self, request):
+        serializer = serializers.TokenSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
