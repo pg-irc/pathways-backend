cbce94921b3135dea473212f198bd0ab8a56ee5e
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-08 16:49:50 -0800

    Issue #441 Add app and model for push notification tokens

---
+++ push_notifications/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.core import validators
+from common.models import RequiredCharField
+
+
+class PushNotificationToken(models.Model):
+    id = RequiredCharField(primary_key=True, max_length=200, validators=[validators.validate_slug])
