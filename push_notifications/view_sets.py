ec945cbdbf144663a5de5193aac3f129b5a60f25
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-13 14:37:03 -0800

    Issue #441 Add documentation

--- push_notifications/view_sets.py
+++ push_notifications/view_sets.py
@@ -4,12 +4,18 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from push_notifications.models import PushNotificationToken
 from push_notifications.serializers import PushNotificationTokenSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 
+@swagger_auto_schema(
+    methods=['PUT'],
+    request_body=PushNotificationTokenSerializer,
+    responses={200: PushNotificationTokenSerializer(many=False)},
+    operation_description='Save a push notification token')
 @api_view(['PUT'])
 def create_or_update_push_notification_token(request, *args, **kwargs):
     data = request.data.copy()
-    data['id'] = kwargs['theid']
+    data['id'] = kwargs['token']
 
     serializer = get_serializer(data)
     if not serializer.is_valid():
