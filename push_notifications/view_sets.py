148ea4885feff10976c3cdccd64ea4af5445b60e
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-13 12:51:26 -0800

    Issue #441 Clean up

--- push_notifications/view_sets.py
+++ push_notifications/view_sets.py
@@ -11,21 +11,23 @@ from django.db.utils import DataError
 def create_or_update_push_notification_token(request, *args, **kwargs):
     the_id = kwargs['theid']
 
-    the_data = request.data.copy()
-    the_data['id'] = the_id
-    serializer = get_serializer(the_id, the_data)
+    data = request.data.copy()
+    data['id'] = the_id
 
+    serializer = get_serializer(data)
     if not serializer.is_valid():
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
     serializer.save()
     return Response(serializer.data)
 
 
-def get_serializer(the_id, the_data):
-    exists = PushNotificationToken.objects.filter(pk=the_id).exists()
+def get_serializer(data):
+    the_id = data['id']
+    create = not PushNotificationToken.objects.filter(pk=the_id).exists()
 
-    if not exists:
-        return serializers.TokenSerializer(data=the_data)
+    if create:
+        return serializers.TokenSerializer(data=data)
 
-    instance = PushNotificationToken.objects.get(pk=the_id)
-    return serializers.TokenSerializer(instance, data=the_data)
+    existing_instance = PushNotificationToken.objects.get(pk=the_id)
+    return serializers.TokenSerializer(existing_instance, data=data)
