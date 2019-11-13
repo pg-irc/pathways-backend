85a22c55d20aa2835746f274b656240e9ec0b820
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-13 14:06:45 -0800

    Issue #441 Clean up

--- push_notifications/serializers.py
+++ push_notifications/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from push_notifications import models
 
 
-class TokenSerializer(serializers.ModelSerializer):
+class PushNotificationTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PushNotificationToken
         fields = '__all__'
--- push_notifications/view_sets.py
+++ push_notifications/view_sets.py
@@ -1,18 +1,15 @@
-from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from rest_framework import status
 from rest_framework.response import Response
-from push_notifications import models, serializers
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import api_view
 from push_notifications.models import PushNotificationToken
-from django.db.utils import DataError
+from push_notifications.serializers import PushNotificationTokenSerializer
 
 
 @api_view(['PUT'])
 def create_or_update_push_notification_token(request, *args, **kwargs):
-    the_id = kwargs['theid']
-
     data = request.data.copy()
-    data['id'] = the_id
+    data['id'] = kwargs['theid']
 
     serializer = get_serializer(data)
     if not serializer.is_valid():
@@ -27,7 +24,7 @@ def get_serializer(data):
     create = not PushNotificationToken.objects.filter(pk=the_id).exists()
 
     if create:
-        return serializers.TokenSerializer(data=data)
+        return PushNotificationTokenSerializer(data=data)
 
     existing_instance = PushNotificationToken.objects.get(pk=the_id)
-    return serializers.TokenSerializer(existing_instance, data=data)
+    return PushNotificationTokenSerializer(existing_instance, data=data)
