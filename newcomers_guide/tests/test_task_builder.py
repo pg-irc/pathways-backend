989719942283ae886bb66fad167a2abe30e89c7e
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-12-03 16:04:59 -0800

    Issue pg-irc/pathways-frontend#789 Remove topic propoerty completed, add property isNewlyRecommended

--- newcomers_guide/tests/test_task_builder.py
+++ newcomers_guide/tests/test_task_builder.py
@@ -9,23 +9,23 @@ class TopicBuilderTest(TestCase):
     def test_can_set_id(self):
         self.topic.set_id('xyz')
         self.assertJSONEqual(self.topic.to_json(),
-                             '{"completed": false, "relatedTopics": [], "id": "xyz"}')
+                             '{"isNewlyRecommended": false, "relatedTopics": [], "id": "xyz"}')
 
     def test_can_set_title_in_a_locale(self):
         self.topic.set_title_in_locale('en', 'xyz')
         self.assertJSONEqual(self.topic.to_json(),
-                             '{"completed": false, "relatedTopics": [], "title": {"en": "xyz"}}')
+                             '{"isNewlyRecommended": false, "relatedTopics": [], "title": {"en": "xyz"}}')
 
     def test_can_set_title_in_multile_locales(self):
         self.topic.set_title_in_locale('en', 'xyz')
         self.topic.set_title_in_locale('fr', 'abc')
         self.assertJSONEqual(self.topic.to_json(),
-                             '{"completed": false, "relatedTopics": [], "title": {"en": "xyz", "fr": "abc"}}')
+                             '{"isNewlyRecommended": false, "relatedTopics": [], "title": {"en": "xyz", "fr": "abc"}}')
 
     def test_can_set_description_in_a_locale(self):
         self.topic.set_description_in_locale('en', 'xyz')
         self.assertJSONEqual(self.topic.to_json(),
-                             '{"completed": false, "relatedTopics": [], "description": {"en": "xyz"}}')
+                             '{"isNewlyRecommended": false, "relatedTopics": [], "description": {"en": "xyz"}}')
 
     def test_can_create_complete_topic(self):
         self.topic.set_id('the topic id').\
@@ -35,7 +35,7 @@ class TopicBuilderTest(TestCase):
             set_description_in_locale('fr', 'the description in French')
 
         expected = ('{'
-                    '"completed": false, '
+                    '"isNewlyRecommended": false, '
                     '"id": "the topic id", '
                     '"title": {"en": "the title in English", "fr": "the title in French"}, '
                     '"description": {"en": "the description in English", "fr": "the description in French"}, '
