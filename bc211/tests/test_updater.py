108a033f7d98c5e876ae74df3144cb357d26c8e2
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-12-30 12:33:07 -0800

    Issue #490 Clean up test for updating phone number

--- bc211/tests/test_updater.py
+++ bc211/tests/test_updater.py
@@ -7,7 +7,7 @@ from human_services.addresses.tests.helpers import AddressBuilder
 from human_services.organizations.tests.helpers import OrganizationBuilder
 from human_services.locations.models import Location, LocationAddress
 from human_services.locations.tests.helpers import LocationBuilder
-from human_services.phone_at_location.models import PhoneAtLocation
+from human_services.phone_at_location.models import PhoneAtLocation, PhoneNumberType
 from common.testhelpers.random_test_values import a_phone_number, a_string
 
 
@@ -17,6 +17,8 @@ class LocationUpdateTests(TestCase):
         self.organization = OrganizationBuilder().create()
         self.physical_address_type = AddressType.objects.get(pk='physical_address')
         self.postal_address_type = AddressType.objects.get(pk='postal_address')
+        self.phone_number_type_id = a_string()
+        self.phone_number_type = PhoneNumberType.objects.create(id=self.phone_number_type_id)
 
     def set_physical_address(self, location, address):
         LocationAddress(address=address, location=location,
@@ -43,26 +45,23 @@ class LocationUpdateTests(TestCase):
         self.assertEqual(all_locations[0].name, new_location_dto.name)
 
     def test_update_phone_number_on_existing_location(self):
-        phone_number_type_id = a_string()
+        location = (LocationBuilder(self.organization).
+                    with_id(self.location_id).
+                    create())
+        PhoneAtLocation.objects.create(phone_number_type=self.phone_number_type,
+                                       phone_number=a_phone_number(),
+                                       location=location)
         new_phone_number = a_phone_number()
+        new_phone_at_location_dto = dtos.PhoneAtLocation(phone_number_type_id=self.phone_number_type_id,
+                                                         phone_number=new_phone_number,
+                                                         location_id=self.location_id)
 
-        phone_at_location = dtos.PhoneAtLocation(phone_number_type_id=phone_number_type_id,
-                                                 phone_number=a_phone_number(),
-                                                 location_id=self.location_id)
-        location_builder = (LocationBuilder(self.organization).
-                            with_id(self.location_id).
-                            with_phone_numbers([phone_at_location]))
+        location_with_new_number = (LocationBuilder(self.organization).
+                                    with_id(self.location_id).
+                                    with_phone_numbers([new_phone_at_location_dto]).
+                                    build_dto())
 
-        save_locations([location_builder.build_dto()], {}, ImportCounters())
-
-        new_phone_at_location = dtos.PhoneAtLocation(phone_number_type_id=phone_number_type_id,
-                                                     phone_number=new_phone_number,
-                                                     location_id=self.location_id)
-        save_locations(
-            [(location_builder.
-              with_phone_numbers([new_phone_at_location]).
-              build_dto())
-             ], {}, ImportCounters())
+        save_locations([location_with_new_number], {}, ImportCounters())
 
         phone_numbers = PhoneAtLocation.objects.all()
         self.assertEqual(len(phone_numbers), 1)
