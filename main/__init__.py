0d09d0228b3185b04533b1c0d16fb6455c69293d
Ian Lin <ian@peacegeeks.org>
2020-01-09 14:10:10 -0800

    Issue PeaceGeeksSociety/content#731 Bump client version to 1.16

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.21'
+__version__ = '1.3.22'
 
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in
                           __version__.replace('-', '.', 1).split('.')])
