c29fe5fb30bbb35b164318a48e1ae83ae055525c
Tomy Reyes <tomy@peacegeeks.org>
2020-01-09 10:00:43 -0800

    Issue #500 renames and cleanup to make linter happy

--- heroku/tests/test_get_version.py
+++ heroku/tests/test_get_version.py
@@ -9,13 +9,13 @@ class TestVersion(TestCase):
         with open('runtime.txt', 'r') as file:
             self.version = file.read().strip()
 
-    def test_python_runtime_version_string_in_heroku_init_py_matches_runtime_dot_txt(self):
+    def test_version_in_heroku_init_py_matches_runtime_txt(self):
         version_string = version.get_python_runtime_version_string()
         self.assertEqual(version_string, self.version)
 
-    def test_python_runtime_version_string_in_heroku_init_py_matches_travis_dot_yml(self):
+    def test_version_in_heroku_init_py_matches_travis_yml(self):
         with open('.travis.yml', 'r') as stream:
             travis_configuration_data = yaml.safe_load(stream)
             travis_python_version = travis_configuration_data['python']
-            formatted_travis_python_version = 'python-{}'.format(travis_python_version)
-            self.assertEqual(formatted_travis_python_version, self.version)
+            prefixed_travis_python_version = 'python-{}'.format(travis_python_version)
+            self.assertEqual(prefixed_travis_python_version, self.version)
