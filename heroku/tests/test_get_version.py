1c296a1a465afe7f2acbb3d053ad77b4d7c16585
Tomy Reyes <tomy@peacegeeks.org>
2020-01-09 09:57:21 -0800

    Issue #500 create unit test for travis python version

--- heroku/tests/test_get_version.py
+++ heroku/tests/test_get_version.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from heroku import version
+import yaml
 
 
 class TestVersion(TestCase):
@@ -11,3 +12,10 @@ class TestVersion(TestCase):
     def test_python_runtime_version_string_in_heroku_init_py_matches_runtime_dot_txt(self):
         version_string = version.get_python_runtime_version_string()
         self.assertEqual(version_string, self.version)
+
+    def test_python_runtime_version_string_in_heroku_init_py_matches_travis_dot_yml(self):
+        with open('.travis.yml', 'r') as stream:
+            travis_configuration_data = yaml.safe_load(stream)
+            travis_python_version = travis_configuration_data['python']
+            formatted_travis_python_version = 'python-{}'.format(travis_python_version)
+            self.assertEqual(formatted_travis_python_version, self.version)
