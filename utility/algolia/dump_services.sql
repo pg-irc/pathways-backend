61f6af8204e9ea614171af19c8c670fc6d1d76c6
rasmus <rasmus@peacegeeks.org>
2020-03-11 13:08:18 -0700

    Issue #509 Update script to add taxonmy data to algolia data

--- utility/algolia/dump_services.sql
+++ utility/algolia/dump_services.sql
@@ -2,11 +2,11 @@
 
 -- sudo npm install --global csvtojson
 
--- psql -d pathways_local -F $',' -A -t -c 'select location_id, phone_number, phone_number_type_id from phone_at_location_phoneatlocation' > phones.csv
--- psql -d pathways_local -F $',' -A -t -c "select s.service_id, t.name from services_service_taxonomy_terms as s, taxonomies_taxonomyterm as t where s.taxonomyterm_id=t.id and t.taxonomy_id like 'bc211-%' " > taxonomyterms.csv
+-- psql -d pathways_local -F $',' -A -t -c 'select location_id, phone_number, phone_number_type_id from phone_at_location_phoneatlocation' > phones_data.csv
+-- psql -d pathways_local -F $',' -A -t -c "select s.service_id, t.name from services_service_taxonomy_terms as s, taxonomies_taxonomyterm as t where s.taxonomyterm_id=t.id and t.taxonomy_id like 'bc211-%' " > taxonomy_data.csv
 -- ubuntu: psql -d pathways_local -F $',' -A -f utility/algolia/dump_services.sql | head -n -1 |                     csvtojson --colParser='{"_geoloc.lng":"number","_geoloc.lat":"number","organization.service_count":"number"}' > services.json
 -- mac   : psql -d pathways_local -F $',' -A -f utility/algolia/dump_services.sql | tail -r | tail -n +2 | tail -r | csvtojson --colParser='{"_geoloc.lng":"number","_geoloc.lat":"number","organization.service_count":"number"}' > services.json
--- python utility/algolia/add_phone_numbers_to_services.py phones.csv services.json services-with-phonenumbers.json
+-- python utility/algolia/add_phone_numbers_to_services.py phones_data.csv taxonomy_data.csv services.json output_data.json
 
 -- Column names _geoloc.lng and _geoloc.lat are special. csvtojson understands to convert this to
 -- "_geoloc":{"lng":-122.724,"lat":49.110044}. The --colParser option define the lat and long to be
