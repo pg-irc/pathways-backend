edeb775cca9f1b29be1492a711f321c67dd13d7f
Ian Lin <ian@peacegeeks.org>
2020-04-20 14:11:12 -0700

    PeaceGeeksSociety/content#746 bump server string

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.27'
+__version__ = '1.3.28'
 
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in
                           __version__.replace('-', '.', 1).split('.')])
