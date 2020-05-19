d98884d0cc42d97d6272d30df57e27ea4c13d870
rasmus <rasmus@peacegeeks.org>
2020-05-19 09:30:11 -0700

    Issue #527 Define ordering on dynamic content to remove warning on pagination inconsisency

--- content/models.py
+++ content/models.py
@@ -11,5 +11,8 @@ class Alert(ValidateOnSaveMixin, TranslatableModel):
     translations = TranslatedFields(title=models.CharField(max_length=200),
                                     description=models.TextField(blank=True, null=True))
 
+    class Meta:
+        ordering = ['id']
+
     def __str__(self):
         return self.title
