ba9bccf3166852c2d2c1a949314e55fbcb4b44fc
rasmus <rasmus@peacegeeks.org>
2020-10-06 09:37:31 -0700

    Issue #557 Use OpenReferral standard names for columns returned from parser

--- bc211/csv_import/parser.py
+++ bc211/csv_import/parser.py
@@ -8,7 +8,7 @@ def parse(lines):
     for row in reader:
         if not len(row):
             break
-        result[-1]['organization_id'] = row[0]
-        result[-1]['organization_name'] = row[1]
+        result[-1]['id'] = row[0]
+        result[-1]['name'] = row[1]
         result.append({})
     return result
--- bc211/csv_import/tests/test_parser.py
+++ bc211/csv_import/tests/test_parser.py
@@ -15,13 +15,13 @@ class ParserTests(TestCase):
         the_id = a_string()
         data = Bc211CsvDataBuilder().with_field(ORGANIZATION_ID_HEADER, the_id).build()
         parsed_data = parse(data)
-        self.assertEqual(parsed_data[0]['organization_id'], the_id)
+        self.assertEqual(parsed_data[0]['id'], the_id)
 
     def test_can_import_organization_name(self):
         the_name = a_string()
         data = Bc211CsvDataBuilder().with_field(ORGANIZATION_NAME_HEADER, the_name).build()
         parsed_data = parse(data)
-        self.assertEqual(parsed_data[0]['organization_name'], the_name)
+        self.assertEqual(parsed_data[0]['name'], the_name)
 
     def test_can_parse_two_organizations(self):
         first_name = a_string()
@@ -30,5 +30,5 @@ class ParserTests(TestCase):
                 with_field(ORGANIZATION_NAME_HEADER, first_name).next_row().
                 with_field(ORGANIZATION_NAME_HEADER, second_name).build())
         parsed_data = parse(data)
-        self.assertEqual(parsed_data[0]['organization_name'], first_name)
-        self.assertEqual(parsed_data[1]['organization_name'], second_name)
+        self.assertEqual(parsed_data[0]['name'], first_name)
+        self.assertEqual(parsed_data[1]['name'], second_name)
