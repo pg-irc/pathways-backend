ca42a562f94a86c40e230a6869f03a4677cfc74f
Tomy Reyes <tomy@peacegeeks.org>
2020-10-23 13:10:14 -0700

    Issue #564 create basic pattern for how csv files will be imported in order

---
+++ bc211/open_referral_csv_import/tests/tests_importer.py
@@ -0,0 +1,10 @@
+import unittest
+from django.test import TestCase
+from ..importer import import_organizations_file
+
+
+class OpenReferralImporterTests(TestCase):
+    def test_missing_organizations_file_throws_exception(self):
+        incorrect_file_path = '../foo/organizations.csv'
+        with self.assertRaises(FileNotFoundError) as error:
+            import_organizations_file(incorrect_file_path)
