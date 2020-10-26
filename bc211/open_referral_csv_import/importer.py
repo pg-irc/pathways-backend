4e9c6adf02324c3fd8fb9ff1cb47396b97a9e284
Tomy Reyes <tomy@peacegeeks.org>
2020-10-26 14:23:40 -0700

    Issue #564 can import organization website

--- bc211/open_referral_csv_import/importer.py
+++ bc211/open_referral_csv_import/importer.py
@@ -41,12 +41,14 @@ def save_organization(organization):
 
 
 def build_active_record(organization):
+    print(organization.website)
     active_record = Organization()
     active_record.id = organization.id
     active_record.name = organization.name
     active_record.alternate_name = organization.alternate_name
     active_record.description = organization.description
     active_record.email = organization.email
+    active_record.website = organization.website
     return active_record
 
 
