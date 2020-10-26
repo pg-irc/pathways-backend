29909cf213f8884e0c3ea968f1c644b551517f2d
Tomy Reyes <tomy@peacegeeks.org>
2020-10-26 09:32:55 -0700

    Issue #564 can parse organization website

--- bc211/open_referral_csv_import/importer.py
+++ bc211/open_referral_csv_import/importer.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from bc211.parser import remove_double_escaped_html_markup
+from urllib import parse as urlparse
 
 LOGGER = logging.getLogger(__name__)
 
@@ -35,6 +36,7 @@ def parse_organization(headers, row):
     alternate_name = row[2]
     description = row[3]
     email = row[4]
+    website = row[5]
     for header in headers:
         if header == 'id':
             organization['id'] = parse_required_field('id', organization_id)
@@ -46,6 +48,8 @@ def parse_organization(headers, row):
             organization['description'] = parse_optional_field('description', description)
         elif header == 'email':
             organization['email'] = parse_optional_field('email', email)
+        elif header == 'url':
+            organization['website'] = parse_website_with_prefix('website', website)
         else:
             continue
     return organization
@@ -63,3 +67,14 @@ def parse_optional_field(field, value):
     if value is None:
         return None
     return remove_double_escaped_html_markup(value)
+
+
+def parse_website_with_prefix(field, value):
+    website = parse_optional_field(field, value)
+    return None if website is None else website_with_http_prefix(website)
+
+
+def website_with_http_prefix(website):
+    parts = urlparse.urlparse(website, 'http')
+    whole_with_extra_slash = urlparse.urlunparse(parts)
+    return whole_with_extra_slash.replace('///', '//')
