86112b6470bf37a3cd47c1af9a6e5deb646170e2
Tomy Reyes <tomy@peacegeeks.org>
2020-10-27 15:05:31 -0700

    Issue #564 add dto validation for services

--- bc211/open_referral_csv_import/tests/test_dtos.py
+++ bc211/open_referral_csv_import/tests/test_dtos.py
@@ -1,7 +1,7 @@
 import unittest
 from django.test import TestCase
 from ..exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException
-from ..dtos import Organization
+from ..dtos import Organization, Service
 
 
 class OrganizationDtoTests(TestCase):
@@ -16,3 +16,21 @@ class OrganizationDtoTests(TestCase):
     def test_throws_on_invalid_type(self):
         with self.assertRaises(InvalidTypeCsvParseException):
             Organization(id='id', name='name', alternate_name=123)
+
+
+class ServiceDtoTests(TestCase):
+    def tests_throws_on_missing_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Service(organization_id='organization_id')
+    
+    def test_throws_on_missing_organization_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Service(id='id')
+    
+    def tests_throws_on_missing_name(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Service(id='id', organization_id='organization_id')
+    
+    def tests_throws_on_invalid_type(self):
+        with self.assertRaises(InvalidTypeCsvParseException):
+            Service(id='id', organization_id='organization_id', name='name', alternate_name=123)
