ea6b07bbb667a3429506bb5d451dc28d4c0d4995
Tomy Reyes <tomy@peacegeeks.org>
2020-10-27 15:05:31 -0700

    Issue #564 add dto validation for services

--- bc211/open_referral_csv_import/dtos.py
+++ bc211/open_referral_csv_import/dtos.py
@@ -9,3 +9,14 @@ class Organization:
         self.description = optional_string('description', kwargs)
         self.website = optional_string('website', kwargs)
         self.email = optional_string('email', kwargs)
+
+
+class Service:
+    def __init__(self, **kwargs):
+        self.id = required_string('id', kwargs)
+        self.organization_id = required_string('organization_id', kwargs)
+        self.name = required_string('name', kwargs)
+        self.alternate_name = optional_string('alternate_name', kwargs)
+        self.description = optional_string('description', kwargs)
+        self.email = optional_string('email', kwargs)
+        self.website = optional_string('website', kwargs)
