2a9cca550e82ad6e49eddc982c7d663463388713
Tomy Reyes <tomy@peacegeeks.org>
2020-10-27 10:14:40 -0700

    Issue #564 can parse service id

--- bc211/open_referral_csv_import/service.py
+++ bc211/open_referral_csv_import/service.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from .parser import parse_required_field
 
 LOGGER = logging.getLogger(__name__)
 
@@ -11,6 +12,21 @@ def import_services_file(root_folder):
         with open(path, 'r') as file: 
             reader = csv.reader(file)
             headers = reader.__next__()
+            for row in reader:
+                if not row:
+                    return
+                service = parse_service(headers, row)
     except FileNotFoundError as error:
             LOGGER.error('Missing services.csv file.')
             raise
+
+
+def parse_service(headers, row):
+    service = {}
+    service_id = row[0]
+    for header in headers:
+        if header == 'id':
+            service['id'] = parse_required_field('id', service_id)
+        else:
+            continue
+    return service
