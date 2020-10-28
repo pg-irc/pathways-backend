9942e8052415ba65e19faac328f398e476a3767c
Tomy Reyes <tomy@peacegeeks.org>
2020-10-28 11:12:10 -0700

    Issue #564 can parse location id

--- bc211/open_referral_csv_import/location.py
+++ bc211/open_referral_csv_import/location.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from .parser import parse_required_field
 
 LOGGER = logging.getLogger(__name__)
 
@@ -11,6 +12,21 @@ def import_locations_file(root_folder):
         with open(path, 'r') as file: 
             reader = csv.reader(file)
             headers = reader.__next__()
+            for row in reader:
+                if not row:
+                    return
+                location = parse_location(headers, row)
     except FileNotFoundError as error:
             LOGGER.error('Missing locations.csv file.')
             raise
+
+
+def parse_location(headers, row):
+    location = {}
+    location_id = row[0]
+    for header in headers:
+        if header == 'id':
+            location['id'] = parse_required_field('id', location_id)
+        else:
+            continue
+    return location 
