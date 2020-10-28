ad7aaf6d92d62c3718b11c6320f55a07ae768391
Tomy Reyes <tomy@peacegeeks.org>
2020-10-28 15:45:40 -0700

    Issue #564 add DTO validation for location

--- bc211/open_referral_csv_import/tests/test_dtos.py
+++ bc211/open_referral_csv_import/tests/test_dtos.py
@@ -1,7 +1,7 @@
 import unittest
 from django.test import TestCase
-from ..exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException
-from ..dtos import Organization, Service
+from ..exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException, InvalidFloatCsvParseException
+from ..dtos import Organization, Service, Location, SpatialLocation
 
 
 class OrganizationDtoTests(TestCase):
@@ -19,7 +19,7 @@ class OrganizationDtoTests(TestCase):
 
 
 class ServiceDtoTests(TestCase):
-    def tests_throws_on_missing_id(self):
+    def test_throws_on_missing_id(self):
         with self.assertRaises(MissingRequiredFieldCsvParseException):
             Service(organization_id='organization_id')
     
@@ -34,3 +34,25 @@ class ServiceDtoTests(TestCase):
     def tests_throws_on_invalid_type(self):
         with self.assertRaises(InvalidTypeCsvParseException):
             Service(id='id', organization_id='organization_id', name='name', alternate_name=123)
+
+
+class LocationDtoTests(TestCase):
+    def test_throws_on_missing_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Location(organization_id='organization_id')
+    
+    def test_throws_on_missing_organization_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Location(id='id')
+    
+    def tests_throws_on_missing_name(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Location(id='id', organization_id='organization_id')
+    
+    def tests_throws_on_invalid_type(self):
+        with self.assertRaises(InvalidTypeCsvParseException):
+            Location(id='id', organization_id='organization_id', name='name', alternate_name=123)
+
+    def tests_throws_on_invalid_spatial_location_type(self):
+        with self.assertRaises(InvalidFloatCsvParseException):
+            Location(id='id', organization_id='organization_id', name='name', spatial_location=SpatialLocation(latitude='foo', longitude='-23.456'))
--- bc211/open_referral_csv_import/validate.py
+++ bc211/open_referral_csv_import/validate.py
@@ -1,4 +1,4 @@
-from .exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException
+from .exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException, InvalidNestedObjectCsvParseException, InvalidFloatCsvParseException
 
 
 def required_string(field, values):
@@ -7,8 +7,28 @@ def required_string(field, values):
         return value
     raise MissingRequiredFieldCsvParseException(field)
 
+
 def optional_string(field, values):
     value = values.get(field)
     if value is None or isinstance(value, str):
         return value
     raise InvalidTypeCsvParseException(field)
+
+
+def optional_object(the_class, field, values):
+    value = values.get(field)
+    if value is None or isinstance(value, the_class):
+        return value
+    raise InvalidNestedObjectCsvParseException(field)
+
+
+def required_float(field, values):
+    value = values.get(field)
+    return parse_float(value)
+
+
+def parse_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        raise InvalidFloatCsvParseException(value)
