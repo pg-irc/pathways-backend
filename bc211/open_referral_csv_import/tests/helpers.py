2911c539a23a1cc81778d23f72525e25943215ed
Tomy Reyes <tomy@peacegeeks.org>
2020-10-30 11:39:08 -0700

    Issue #564 simplify organization csv builder

--- bc211/open_referral_csv_import/tests/helpers.py
+++ bc211/open_referral_csv_import/tests/helpers.py
@@ -5,48 +5,44 @@ from bc211.open_referral_csv_import import dtos
 
 class OpenReferralCsvOrganizationBuilder:
     def __init__(self):
-        self.data = self.a_row()
-
-    def a_row(self):
-        organization_id = a_string()
-        name = a_string()
-        alternate_name = a_string()
-        description = a_string()
-        email = an_email_address()
-        url = a_website_address()
-        not_used_tax_status = a_string()
-        not_used_tax_id = a_string()
-        not_used_year_incorporated = a_string()
-        not_used_legal_status = a_string()
-        return [organization_id, name, alternate_name, description, email, url,
-                not_used_tax_status, not_used_tax_id, not_used_year_incorporated, not_used_legal_status]
+        self.organization_id = a_string()
+        self.name = a_string()
+        self.alternate_name = a_string()
+        self.description = a_string()
+        self.email = an_email_address()
+        self.url = a_website_address()
+        self.not_used_tax_status = a_string()
+        self.not_used_tax_id = a_string()
+        self.not_used_year_incorporated = a_string()
+        self.not_used_legal_status = a_string()
     
     def with_id(self, organization_id):
-        self.data[0] = organization_id
+        self.organization_id = organization_id
         return self
 
     def with_name(self, name):
-        self.data[1] = name
+        self.name = name
         return self
 
     def with_alternate_name(self, alternate_name):
-        self.data[2] = alternate_name
+        self.alternate_name = alternate_name
         return self
     
     def with_description(self, description):
-        self.data[3] = description
+        self.description = description
         return self
 
     def with_email(self, email):
-        self.data[4] = email
+        self.email = email
         return self
 
     def with_url(self, url):
-        self.data[5] = url
+        self.url = url
         return self
         
     def build(self):
-        return self.data
+        return [self.organization_id, self.name, self.alternate_name, self.description, self.email, self.url,
+                self.not_used_tax_status, self.not_used_tax_id, self.not_used_year_incorporated, self.not_used_legal_status]
 
 
 class OpenReferralCsvServiceBuilder:
