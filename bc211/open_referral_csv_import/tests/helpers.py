9ce672af93fcdb0590ba16ce67a8202b2dd095be
Tomy Reyes <tomy@peacegeeks.org>
2020-10-30 11:49:36 -0700

    Issue #564 simplify service csv builder

--- bc211/open_referral_csv_import/tests/helpers.py
+++ bc211/open_referral_csv_import/tests/helpers.py
@@ -55,60 +55,55 @@ class OpenReferralCsvOrganizationBuilder:
 
 class OpenReferralCsvServiceBuilder:
     def __init__(self, organization):
-        self.data = self.a_row(organization)
-
-    def a_row(self, organization):
-        service_id = a_string()
-        organization_id = organization.id
-        not_used_program_id = a_string()
-        name = a_string()
-        alternate_name = a_string()
-        description = a_string()
-        url = a_website_address()
-        email = an_email_address()
-        not_used_status = a_string()
-        not_used_intepretation_services = a_string()
-        not_used_application_process = a_string()
-        not_used_wait_time = a_string()
-        not_used_fees = a_string()
-        not_used_accreditations = a_string()
-        not_used_licenses = a_string()
-        not_used_taxonomy_ids = a_string()
-
-        return [service_id, organization_id, not_used_program_id, name, alternate_name, description, url, email,
-                not_used_status, not_used_intepretation_services, not_used_application_process, not_used_wait_time,
-                not_used_fees, not_used_accreditations, not_used_licenses, not_used_taxonomy_ids]
+        self.service_id = a_string()
+        self.organization_id = organization.id
+        self.not_used_program_id = a_string()
+        self.name = a_string()
+        self.alternate_name = a_string()
+        self.description = a_string()
+        self.url = a_website_address()
+        self.email = an_email_address()
+        self.not_used_status = a_string()
+        self.not_used_intepretation_services = a_string()
+        self.not_used_application_process = a_string()
+        self.not_used_wait_time = a_string()
+        self.not_used_fees = a_string()
+        self.not_used_accreditations = a_string()
+        self.not_used_licenses = a_string()
+        self.not_used_taxonomy_ids = a_string()
     
     def with_id(self, service_id):
-        self.data[0] = service_id
+        self.service_id = service_id
         return self
 
     def with_organization_id(self, organization_id):
-        self.data[1] = organization_id
+        self.organization_id = organization_id
         return self
 
     def with_name(self, name):
-        self.data[3] = name
+        self.name = name
         return self
     
     def with_alternate_name(self, alternate_name):
-        self.data[4] = alternate_name
+        self.alternate_name = alternate_name
         return self
     
     def with_description(self, description):
-        self.data[5] = description
+        self.description = description
         return self
 
     def with_url(self, url):
-        self.data[6] = url
+        self.url = url
         return self
 
     def with_email(self, email):
-        self.data[7] = email
+        self.email = email
         return self
     
     def build(self):
-        return self.data
+        return [self.service_id, self.organization_id, self.not_used_program_id, self.name, self.alternate_name, self.description, self.url, self.email,
+                self.not_used_status, self.not_used_intepretation_services, self.not_used_application_process, self.not_used_wait_time,
+                self.not_used_fees, self.not_used_accreditations, self.not_used_licenses, self.not_used_taxonomy_ids]
 
 
 class OpenReferralCsvLocationBuilder:
