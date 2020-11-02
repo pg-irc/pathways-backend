23ef07a20e2e695576441d06a0f899b0c7ebc1b7
Tomy Reyes <tomy@peacegeeks.org>
2020-11-02 15:09:40 -0800

    Issue #564 include Address DTO validation

--- bc211/open_referral_csv_import/tests/test_dtos.py
+++ bc211/open_referral_csv_import/tests/test_dtos.py
@@ -1,7 +1,7 @@
 import unittest
 from django.test import TestCase
 from ..exceptions import MissingRequiredFieldCsvParseException, InvalidTypeCsvParseException, InvalidFloatCsvParseException
-from ..dtos import Organization, Service, Location, SpatialLocation, ServiceAtLocation
+from ..dtos import Organization, Service, Location, SpatialLocation, ServiceAtLocation, Address
 
 
 class OrganizationDtoTests(TestCase):
@@ -66,3 +66,29 @@ class ServiceAtLocationDtoTests(TestCase):
     def test_throws_on_missing_location_id(self):
         with self.assertRaises(MissingRequiredFieldCsvParseException):
             ServiceAtLocation(service_id='service_id')
+
+
+class AddressDtoTests(TestCase):
+    def test_throws_on_missing_address_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Address(type='postal_address', location_id='location_id', city='city', country='country')
+    
+    def test_throws_on_missing_type(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Address(id='id', location_id='location_id', city='city', country='country')
+
+    def test_throws_on_missing_location_id(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Address(id='id', type='postal_address', city='city', country='country')
+
+    def test_throws_on_missing_city(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Address(id='id', type='postal_address', location_id='location_id', country='country')
+
+    def test_throws_on_missing_country(self):
+        with self.assertRaises(MissingRequiredFieldCsvParseException):
+            Address(id='id', type='postal_address', location_id='location_id', city='city')
+    
+    def tests_throws_on_invalid_type(self):
+        with self.assertRaises(InvalidTypeCsvParseException):
+            Address(id='id', type='postal_address', location_id='location_id', city='city', country='country', address=123)
