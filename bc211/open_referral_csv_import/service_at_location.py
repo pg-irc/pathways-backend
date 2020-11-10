3f9e360544ad5245563f068656dc1635cbb26dc6
Tomy Reyes <tomy@peacegeeks.org>
2020-11-10 15:59:32 -0800

    Issue #564 remove unneeded loop and parser tests for service at location

--- bc211/open_referral_csv_import/service_at_location.py
+++ bc211/open_referral_csv_import/service_at_location.py
@@ -17,24 +17,17 @@ def import_services_at_location_file(root_folder):
             for row in reader:
                 if not row:
                     return
-                service_at_location = parse_service_at_location(headers, row)
+                service_at_location = parse_service_at_location(row)
                 save_service_at_location(service_at_location)
     except FileNotFoundError as error:
             LOGGER.error('Missing services_at_location.csv file.')
             raise
 
 
-def parse_service_at_location(headers, row):
+def parse_service_at_location(row):
     service_at_location = {}
-    service_id = row[1]
-    location_id = row[2]
-    for header in headers:
-        if header == 'service_id':
-            service_at_location['service_id'] = parse_required_field('service_id', service_id)
-        elif header == 'location_id':
-            service_at_location['location_id'] = parse_required_field('location_id', location_id)
-        else: 
-            continue
+    service_at_location['service_id'] = parse_required_field('service_id', row[1])
+    service_at_location['location_id'] = parse_required_field('location_id', row[2])
     return dtos.ServiceAtLocation(service_id=service_at_location['service_id'],
                                 location_id=service_at_location['location_id'])
 
