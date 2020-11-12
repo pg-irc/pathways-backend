aa5f9bcf8f9cb9119d4fc4fd4a28bbd46b3e7f21
Tomy Reyes <tomy@peacegeeks.org>
2020-11-12 14:58:13 -0800

    Issue #564 import phone number type

--- bc211/open_referral_csv_import/phones.py
+++ bc211/open_referral_csv_import/phones.py
@@ -2,6 +2,8 @@ import os
 import logging
 from bc211.open_referral_csv_import import headers_match_expected_format
 from bc211.open_referral_csv_import.exceptions import InvalidFileCsvImportException
+from human_services.phone_at_location.models import PhoneNumberType
+from bc211.open_referral_csv_import import parser
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,6 +17,10 @@ def import_phones_file(root_folder):
             headers = reader.__next__()
             if not headers_match_expected_format(headers, expected_headers):
                 raise InvalidFileCsvImportException('The headers in "{0}": does not match open referral standards.'.format(field))
+            for row in reader:
+                if not row:
+                    return
+                import_phone(row)
     except FileNotFoundError as error:
             LOGGER.error('Missing phones.csv file.')
             raise
@@ -22,3 +28,14 @@ def import_phones_file(root_folder):
 
 expected_headers = ['id', 'location_id', 'service_id', 'organization_id', 'contact_id', 'service_at_location_id',
                   'number', 'extension', 'type', 'language', 'description', 'department']
+
+
+def import_phone(row):
+    phone_number_type_active_record = build_phone_number_type_active_record(row)
+    phone_number_type_active_record.save()
+
+
+def build_phone_number_type_active_record(row):
+    active_record = PhoneNumberType()
+    active_record.id = parser.parse_required_type(row[8])
+    return active_record
