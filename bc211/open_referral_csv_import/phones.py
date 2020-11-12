892c24e227db292bdf9b1f69ac75c7e74aaecad4
Tomy Reyes <tomy@peacegeeks.org>
2020-11-12 14:17:22 -0800

    Issue #564 check to see if headers in phones csv are what is expected

--- bc211/open_referral_csv_import/phones.py
+++ bc211/open_referral_csv_import/phones.py
@@ -1,5 +1,7 @@
 import os
 import logging
+from bc211.open_referral_csv_import import headers_match_expected_format
+from bc211.open_referral_csv_import.exceptions import InvalidFileCsvImportException
 
 LOGGER = logging.getLogger(__name__)
 
@@ -11,6 +13,12 @@ def import_phones_file(root_folder):
         with open(path, 'r') as file: 
             reader = csv.reader(file)
             headers = reader.__next__()
+            if not headers_match_expected_format(headers, expected_headers):
+                raise InvalidFileCsvImportException('The headers in "{0}": does not match open referral standards.'.format(field))
     except FileNotFoundError as error:
             LOGGER.error('Missing phones.csv file.')
             raise
+
+
+expected_headers = ['id', 'location_id', 'service_id', 'organization_id', 'contact_id', 'service_at_location_id',
+                  'number', 'extension', 'type', 'language', 'description', 'department']
