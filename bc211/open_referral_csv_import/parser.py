85639c4f0996d8f94d4ea1d0a5adc775150717d8
Tomy Reyes <tomy@peacegeeks.org>
2020-11-12 14:04:15 -0800

    Issue #564 remove unused argument from parser function

--- bc211/open_referral_csv_import/parser.py
+++ bc211/open_referral_csv_import/parser.py
@@ -25,22 +25,22 @@ def parse_name(value):
 
 
 def parse_alternate_name(value):
-    alternate_name = parse_optional_field('alternate_name', value)
+    alternate_name = parse_optional_field(value)
     return remove_double_escaped_html_markup(alternate_name)
 
 
 def parse_description(value):
-    description = parse_optional_field('description', value)
+    description = parse_optional_field(value)
     return remove_double_escaped_html_markup(description)
 
 
 def parse_email(value):
-    email = parse_optional_field('email', value)
+    email = parse_optional_field(value)
     return remove_double_escaped_html_markup(email)
 
 
 def parse_last_verified_date(value):
-    last_verified_date = parse_optional_field('last_verified_date', value)
+    last_verified_date = parse_optional_field(value)
     if last_verified_date is None or value == '':
         return None
     return datetime.strptime(last_verified_date, '%d-%m-%Y')
@@ -52,12 +52,12 @@ def parse_required_type(value):
 
 
 def parse_attention(value):
-    attention = parse_optional_field('attention', value)
+    attention = parse_optional_field(value)
     return remove_double_escaped_html_markup(attention)
 
 
 def parse_address(value):
-    address = parse_optional_field('address',value)
+    address = parse_optional_field(value)
     return remove_double_escaped_html_markup(address)
 
 
@@ -67,12 +67,12 @@ def parse_city(value):
 
 
 def parse_state_province(value):
-    state_province = parse_optional_field('state_province', value)
+    state_province = parse_optional_field(value)
     return remove_double_escaped_html_markup(state_province)
 
 
 def parse_postal_code(value):
-    postal_code = parse_optional_field('postal_code', value)
+    postal_code = parse_optional_field(value)
     return remove_double_escaped_html_markup(postal_code)
 
 
@@ -86,16 +86,15 @@ def parse_required_field(field, value):
         raise MissingRequiredFieldCsvParseException('Missing required field: "{0}"'.format(field))
     return value
 
-# TODO remove field argument
 
-def parse_optional_field(field, value):
+def parse_optional_field(value):
     if csv_value_is_empty(value):
         return None
     return value
 
 
-def parse_website_with_prefix(field, value):
-    website = parse_optional_field(field, value)
+def parse_website_with_prefix(value):
+    website = parse_optional_field(value)
     return None if csv_value_is_empty(value) else website_with_http_prefix(website)
 
 
@@ -104,9 +103,11 @@ def website_with_http_prefix(website):
     whole_with_extra_slash = urlparse.urlunparse(parts)
     return whole_with_extra_slash.replace('///', '//')
 
-def parse_coordinate_if_defined(field, value):
-    coordinate = parse_optional_field(field, value)
+
+def parse_coordinate_if_defined(value):
+    coordinate = parse_optional_field(value)
     return None if csv_value_is_empty(value) else float(coordinate)
+
 
 def csv_value_is_empty(value):
     return value is None or value == ''
