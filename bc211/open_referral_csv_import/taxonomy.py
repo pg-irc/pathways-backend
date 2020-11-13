c4d5ce19db5e7ef603809fdcb26ccab6edf50816
Tomy Reyes <tomy@peacegeeks.org>
2020-11-13 11:02:47 -0800

    Issue #564 check to see if headers in taxonomy csv are what is expected

--- bc211/open_referral_csv_import/taxonomy.py
+++ bc211/open_referral_csv_import/taxonomy.py
@@ -1,5 +1,7 @@
 import os
 import logging
+from bc211.open_referral_csv_import import headers_match_expected_format
+from bc211.open_referral_csv_import.exceptions import InvalidFileCsvImportException
 
 LOGGER = logging.getLogger(__name__)
 
@@ -11,6 +13,11 @@ def import_taxonomy_file(root_folder):
         with open(path, 'r') as file: 
             reader = csv.reader(file)
             headers = reader.__next__()
+            if not headers_match_expected_format(headers, expected_headers):
+                raise InvalidFileCsvImportException('The headers in "{0}": does not match open referral standards.'.format(field))
     except FileNotFoundError as error:
             LOGGER.error('Missing taxonomy.csv file.')
             raise
+
+
+expected_headers = ['id', 'name', 'parent_id', 'parent_name', 'vocabulary']
