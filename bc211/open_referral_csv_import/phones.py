47be184bcb2eab6d2422e5af3f7bb2b88c4aa6f9
Tomy Reyes <tomy@peacegeeks.org>
2020-11-13 10:41:43 -0800

    Issue #564 can import required phone at location fields

--- bc211/open_referral_csv_import/phones.py
+++ bc211/open_referral_csv_import/phones.py
@@ -2,7 +2,7 @@ import os
 import logging
 from bc211.open_referral_csv_import import headers_match_expected_format
 from bc211.open_referral_csv_import.exceptions import InvalidFileCsvImportException
-from human_services.phone_at_location.models import PhoneNumberType
+from human_services.phone_at_location.models import PhoneNumberType, PhoneAtLocation
 from bc211.open_referral_csv_import import parser
 
 LOGGER = logging.getLogger(__name__)
@@ -33,9 +33,20 @@ expected_headers = ['id', 'location_id', 'service_id', 'organization_id', 'conta
 def import_phone(row):
     phone_number_type_active_record = build_phone_number_type_active_record(row)
     phone_number_type_active_record.save()
+    phone_at_location_active_record = build_phone_at_location_active_record(row)
+    phone_at_location_active_record.save()
 
 
 def build_phone_number_type_active_record(row):
     active_record = PhoneNumberType()
     active_record.id = parser.parse_required_type(row[8])
+    return active_record
+
+
+def build_phone_at_location_active_record(row):
+    active_record = PhoneAtLocation()
+    active_record.location_id = parser.parse_location_id(row[1])
+    active_record.phone_number = parser.parse_phone_number(row[6])
+    phone_type = parser.parse_required_type(row[8])
+    active_record.phone_number_type = PhoneNumberType.objects.get(pk=phone_type)
     return active_record
