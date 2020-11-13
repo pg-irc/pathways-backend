a227e69471befe47e56872e9aaa9595eff2d5fd9
Tomy Reyes <tomy@peacegeeks.org>
2020-11-13 11:00:52 -0800

    Issue #564 import taxonomy csv

--- bc211/open_referral_csv_import/importer.py
+++ bc211/open_referral_csv_import/importer.py
@@ -5,6 +5,7 @@ from bc211.open_referral_csv_import.location import import_locations_file
 from bc211.open_referral_csv_import.services_at_location import import_services_at_location_file
 from bc211.open_referral_csv_import.address import import_addresses_file
 from bc211.open_referral_csv_import.phone import import_phones_file
+from bc211.open_referral_csv_import.taxonomy import import_taxonomy_file
 
 
 LOGGER = logging.getLogger(__name__)
@@ -19,5 +20,6 @@ def import_open_referral_files(root_folder):
         import_services_at_location_file(root_folder)
         import_addresses_file(root_folder)
         import_phones_file(root_folder)
+        import_taxonomy_file(root_folder)
     except Exception as error:
         LOGGER.error(error)
