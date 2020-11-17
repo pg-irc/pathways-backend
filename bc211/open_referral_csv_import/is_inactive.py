96d98b9e257f5c7afee12d283647d5fd37c9f65e
Tomy Reyes <tomy@peacegeeks.org>
2020-11-17 09:47:33 -0800

    Issue #564 improve is_inactive logic to account for differences in casing

--- bc211/open_referral_csv_import/is_inactive.py
+++ bc211/open_referral_csv_import/is_inactive.py
@@ -1,3 +1,9 @@
+import re
+
 def is_inactive(description):
     # This is BC211's convention for marking records as inactive
-    return description and description.strip().startswith('DEL')
+    return description and description_starts_with_inactive_record_convention(description)
+
+def description_starts_with_inactive_record_convention(description):
+    stripped_description = description.strip()
+    return re.search(r'^[D-d-E-e-L-l]{3}[0-9]', stripped_description)
