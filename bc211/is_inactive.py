6050a29b69b27f7166a217d06d458b463b6a82e2
Tomy Reyes <tomy@peacegeeks.org>
2020-11-20 10:18:17 -0800

    Issue #564 add outlier case for is_inactive

--- bc211/is_inactive.py
+++ bc211/is_inactive.py
@@ -1,6 +1,6 @@
 import re
 
-# BC211's convention for marking records as inactive is DEL + number
+# BC211's convention for marking records as inactive is DEL + number or DEL + whitespace + number
 
 def is_inactive(description):
     return description and description_starts_with_inactive_record_convention(description)
@@ -8,4 +8,4 @@ def is_inactive(description):
 
 def description_starts_with_inactive_record_convention(description):
     stripped_description = description.strip()
-    return re.search(r'^[D-d-E-e-L-l]{3}[0-9]', stripped_description)
+    return re.search(r'^[D-d-E-e-L-l]{3}[0-9\s]', stripped_description)
