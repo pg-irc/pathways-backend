1ea1eca118dfe352f79222f2f992aa2769ccd52e
Tomy Reyes <tomy@peacegeeks.org>
2020-11-30 16:10:50 -0800

    Issue #564 move collector instantiation to parent

--- bc211/management/commands/import_open_referral_csv_data.py
+++ bc211/management/commands/import_open_referral_csv_data.py
@@ -1,6 +1,7 @@
 import argparse
 from django.core.management.base import BaseCommand
 from bc211.open_referral_csv_import.importer import import_open_referral_files
+from bc211.open_referral_csv_import.inactive_records_collector import InactiveRecordsCollector
 
 # invoke as follows:
 # python manage.py import_open_referral_csv_data path/to/open/referral/files
@@ -17,4 +18,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         root_folder = options['path']
         self.stdout.write('Importing open referral CSV data from {}'.format(root_folder))
-        import_open_referral_files(root_folder)
+        collector = InactiveRecordsCollector()
+        import_open_referral_files(root_folder, collector)
--- bc211/open_referral_csv_import/importer.py
+++ bc211/open_referral_csv_import/importer.py
@@ -1,5 +1,4 @@
 import logging
-from bc211.open_referral_csv_import.inactive_records_collector import InactiveRecordsCollector
 from bc211.open_referral_csv_import.organization import import_organizations_file
 from bc211.open_referral_csv_import.service import import_services_file
 from bc211.open_referral_csv_import.location import import_locations_file
@@ -11,8 +10,7 @@ from bc211.open_referral_csv_import.service_taxonomy import import_services_taxo
 
 # TODO add spatial location to locations where missing when addresses file is imported
 
-def import_open_referral_files(root_folder):
-    collector = InactiveRecordsCollector()
+def import_open_referral_files(root_folder, collector):
     import_organizations_file(root_folder, collector)
     import_services_file(root_folder, collector)
     import_locations_file(root_folder, collector)
