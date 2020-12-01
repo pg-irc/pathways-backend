7572eba6bd6e05814484f95c19bee19cbf5b3f30
Tomy Reyes <tomy@peacegeeks.org>
2020-12-01 12:07:30 -0800

    Issue #564 add helper functions to make phone import more readable

--- bc211/open_referral_csv_import/phone.py
+++ bc211/open_referral_csv_import/phone.py
@@ -17,19 +17,27 @@ LOGGER = logging.getLogger(__name__)
 def import_phones_file(root_folder, collector, counters):
     filename = 'phones.csv'
     path = os.path.join(root_folder, filename)
-    with open(path, 'r') as file: 
+    read_file(path, collector, counters)
+
+
+def read_file(path, collector, counters):
+     with open(path, 'r') as file: 
         reader = csv.reader(file)
         headers = reader.__next__()
         if not headers_match_expected_format(headers, expected_headers):
             raise InvalidFileCsvImportException('The headers in "{0}": does not match open referral standards.'.format(field))
-        for row in reader:
-            if not row:
-                continue
-            import_phone(row, collector, counters)
-
+        read_and_import_rows(reader, collector, counters)
+        
 
 expected_headers = ['id', 'location_id', 'service_id', 'organization_id', 'contact_id', 'service_at_location_id',
                   'number', 'extension', 'type', 'language', 'description', 'department']
+
+
+def read_and_import_rows(reader, collector, counters):
+    for row in reader:
+        if not row:
+            continue
+        import_phone(row, collector, counters)
 
 
 def import_phone(row, collector, counters):
