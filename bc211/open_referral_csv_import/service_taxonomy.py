9b594df9a6354765ecc4a35809c406b281f0b85d
Tomy Reyes <tomy@peacegeeks.org>
2020-12-04 16:15:27 -0800

    Issue #564 break out helper functions for services taxonomy optimization

--- bc211/open_referral_csv_import/service_taxonomy.py
+++ bc211/open_referral_csv_import/service_taxonomy.py
@@ -44,26 +44,49 @@ def read_and_import_rows(reader, collector):
 def import_service_taxonomy(row, last_service_active_record, service_taxonomies_update_list):
     try:
         last_service_id = last_service_active_record.id if last_service_active_record else None
+        current_active_record = None
         current_service_id = parser.parse_service_id(row[1])
         taxonomy_id = parser.parse_taxonomy_id(row[2])
         taxonomy_term = get_taxonomy_term_active_record_or_raise(taxonomy_id)
-        current_active_record = None
 
         if current_service_id is last_service_id:
-            last_service_active_record.taxonomy_term.add(taxonomy_term)
-            service_taxonomies_update_list.append(last_service_active_record)
+            add_service_taxonomy_update_to_list(
+                taxonomy_term,
+                last_service_active_record,
+                service_taxonomies_update_list
+            )
         else:
-            if service_taxonomies_update_list:
-                Service.objects.bulk_update(service_taxonomies_update_list)
+            bulk_update_service_taxonomies_update_list(service_taxonomies_update_list)
+            set_last_service_to_current_active_record(
+                current_service_id,
+                taxonomy_term,
+                last_service_active_record
+            )
             service_taxonomies_update_list.clear()
-            current_active_record = build_service_taxonomy_active_record(current_service_id, taxonomy_term)
-            last_service_active_record = current_active_record 
     except ValidationError as error:
         LOGGER.warn('{}'.format(error.__str__()))
     except ObjectDoesNotExist as error:
         pass
-        
-        
+
+
+def add_service_taxonomy_update_to_list(taxonomy_term, last_service_active_record, service_taxonomies_update_list):
+    last_service_active_record.taxonomy_term.add(taxonomy_term)
+    service_taxonomies_update_list.append(last_service_active_record)
+
+
+def bulk_update_service_taxonomies_update_list(service_taxonomies_update_list):
+    if service_taxonomies_update_list:
+        Service.objects.bulk_update(service_taxonomies_update_list)
+
+
+def set_last_service_to_current_active_record(current_service_id, taxonomy_term, last_service_active_record):
+    current_active_record = build_service_taxonomy_active_record(
+        current_service_id,
+        taxonomy_term
+    )
+    last_service_active_record = current_active_record 
+
+
 def build_service_taxonomy_active_record(service_id, taxonomy_term):
     service_active_record = get_service_active_record_or_raise(service_id)
     service_active_record.taxonomy_terms.add(taxonomy_term)
