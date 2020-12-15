e943491515acdddc2ac33b78795572678e99db10
Tomy Reyes <tomy@peacegeeks.org>
2020-12-14 16:18:02 -0800

    Issue #564 reorganize exception handling in services taxonomy

--- bc211/open_referral_csv_import/service_taxonomy.py
+++ bc211/open_referral_csv_import/service_taxonomy.py
@@ -40,7 +40,7 @@ def read_and_import_rows(reader, collector):
         service_id = parser.parse_required_field_with_double_escaped_html('service_id', row[1])
         if collector.has_inactive_service_id(service_id):
             continue
-        service = get_service_active_record_or_raise(service_id)
+        service = Service.objects.get(pk=service_id)
         import_service_taxonomy(row, service)
 
 
@@ -51,27 +51,15 @@ def import_service_taxonomy(row, service):
         service.taxonomy_terms.add(taxonomy_term)
     except ValidationError as error:
         LOGGER.warning('%s', error.__str__())
-    except ObjectDoesNotExist:
-        pass
+    except ObjectDoesNotExist as error:
+        LOGGER.warning('%s', error.__str__())
 
 
 def build_service_taxonomy_active_record(service_id, taxonomy_term):
-    service_active_record = get_service_active_record_or_raise(service_id)
+    service_active_record = Service.objects.get(pk=service_id)
     service_active_record.taxonomy_terms.add(taxonomy_term)
     return service_active_record
 
 
-def get_service_active_record_or_raise(service_id):
-    try:
-        return Service.objects.get(pk=service_id)
-    except ObjectDoesNotExist as error:
-        LOGGER.warning('Service record with id "%s" does not exist. %s', service_id, error)
-        raise
-
-
 def get_taxonomy_term_active_record_or_raise(taxonomy_id):
-    try:
-        return TaxonomyTerm.objects.get(taxonomy_id=taxonomy_id)
-    except ObjectDoesNotExist:
-        LOGGER.warning('Taxonomy record with id "%s" does not exist.', taxonomy_id)
-        raise
+    return TaxonomyTerm.objects.get(taxonomy_id=taxonomy_id)
