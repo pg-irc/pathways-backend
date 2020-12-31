f5dcd36723727c5f3557737711bd0f0b551d49eb
rasmus <rasmus@peacegeeks.org>
2020-12-31 13:48:54 -0800

    Issue #564 Write end to end test for service with address

--- bc211/import_open_referral_csv/tests/test_end_to_end.py
+++ bc211/import_open_referral_csv/tests/test_end_to_end.py
@@ -2,18 +2,159 @@ from django.test import TestCase
 from bc211.import_open_referral_csv.inactive_records_collector import InactiveRecordsCollector
 from bc211.import_icarol_xml.import_counters import ImportCounters
 from bc211.import_open_referral_csv.organization import read_and_import_rows as import_organization
+from bc211.import_open_referral_csv.location import read_and_import_row as import_locations
 from bc211.import_open_referral_csv.service import read_and_import_rows as import_services
+from bc211.import_open_referral_csv.address import read_and_import_rows as import_addresses
 from bc211.import_open_referral_csv.taxonomy import read_and_import_rows as import_taxonomy_terms
 from bc211.import_open_referral_csv.service_taxonomy import read_and_import_rows as import_service_taxonomy
 from human_services.organizations.models import Organization
 from human_services.services.models import Service
+from human_services.addresses.models import Address
 from taxonomies.models import TaxonomyTerm
 from bc211.convert_icarol_csv.tests.helpers import Bc211CsvDataBuilder, TestDataSink
 from bc211.convert_icarol_csv.parser import parse
 from common.testhelpers.random_test_values import a_string
+from human_services.locations.models import Location
 
 
 class TaxonomyEndToEndTest(TestCase):
+
+    def test_service_with_two_addresses(self):
+        the_organization_id = a_string()
+        the_service_id = a_string()
+        physical_address_1 = a_string()
+        physical_city = a_string()
+        mailing_address_1 = a_string()
+        mailing_city = a_string()
+
+        # create and parse the bc211 csv data for a service with mailing and physical address
+
+        bc211_csv_data = (Bc211CsvDataBuilder().
+                          as_organization().
+                          with_field('ResourceAgencyNum', the_organization_id).
+                          with_field('PublicName', a_string()).
+                          next_row().
+                          as_service().
+                          with_field('ResourceAgencyNum', the_service_id).
+                          with_field('PublicName', a_string()).
+                          with_field('ParentAgencyNum', the_organization_id).
+                          with_field('MailingAddress1', mailing_address_1).
+                          with_field('MailingAddress2', a_string()).
+                          with_field('MailingAddress3', a_string()).
+                          with_field('MailingCity', mailing_city).
+                          with_field('MailingStateProvince', a_string()).
+                          with_field('MailingCountry', 'CA').
+                          with_field('PhysicalAddress1', physical_address_1).
+                          with_field('PhysicalAddress2', a_string()).
+                          with_field('PhysicalAddress3', a_string()).
+                          with_field('PhysicalCity', physical_city).
+                          with_field('PhysicalStateProvince', a_string()).
+                          with_field('PhysicalCountry', 'CA').
+                          build())
+        parsed_data = parse(TestDataSink(), bc211_csv_data)
+
+        self.assertEqual(len(parsed_data.organizations), 1)
+        self.assertEqual(parsed_data.organizations[0]['id'], the_organization_id)
+
+        self.assertEqual(len(parsed_data.services), 1)
+        self.assertEqual(parsed_data.services[0]['organization_id'], the_organization_id)
+        self.assertEqual(parsed_data.services[0]['id'], the_service_id)
+
+        self.assertEqual(len(parsed_data.locations), 2) # deal with the 2
+        location_id = parsed_data.locations[1]['id']
+
+        addresses = parsed_data.addresses
+        self.assertEqual(len(addresses), 2)
+        if addresses[0]['type'] == 'physical_address':
+            physical_address = addresses[0]
+            mailing_address = addresses[1]
+        else:
+            mailing_address = addresses[0]
+            physical_address = addresses[1]
+
+        self.assertEqual(physical_address['address_1'], physical_address_1)
+        self.assertEqual(mailing_address['address_1'], mailing_address_1)
+
+        # import organization
+
+        open_referral_csv_data = [[
+            # id,name,alternate_name,
+            r['id'], r['name'], r['alternate_name'],
+            # description,email,url,
+            '', '', '',
+            # tax_status,tax_id,year_incorporated,legal_status
+            '', '', '', '']
+                for r in parsed_data.organizations]
+        collector = InactiveRecordsCollector()
+        counters = ImportCounters()
+        import_organization(open_referral_csv_data, collector, counters)
+        result = Organization.objects.all()
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].id, the_organization_id)
+
+        # import location
+
+        open_referral_csv_data = [[
+            # id,organization_id,name,
+            r['id'], the_organization_id, r['name'],
+            # alternate_name,description,transportation,
+            '', '', '',
+            # latitude,longitude
+            0, 0]
+                for r in parsed_data.locations]
+        import_locations(open_referral_csv_data, collector, counters)
+        result = Location.objects.all()
+        self.assertEqual(len(result), 2)
+
+        # import service
+
+        open_referral_csv_data = [[
+            # id,organization_id,program_id,
+            r['id'], r['organization_id'], '',
+            # name,alternate_name,description,
+            r['name'], r['alternate_name'], '',
+            # url,email,status,
+            r['url'], r['email'], '',
+            # interpretation_services,application_process,wait_time,
+            '', '', '',
+            # fees,accreditations,licenses, taxonomy_ids,last_verified_on-x
+            '', '', '', '', '']
+                for r in parsed_data.services]
+        import_services(open_referral_csv_data, collector, counters)
+        result = Service.objects.all()
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].id, the_service_id)
+        self.assertEqual(result[0].organization_id, the_organization_id)
+
+        # import addresses
+
+        open_referral_csv_data = [[
+            # id,type,location_id,
+            r['id'], r['type'], location_id,
+            # attention,address_1,address_2,
+            '', r['address_1'], r['address_2'],
+            # address_3,address_4,city,
+            r['address_3'], '', r['city'],
+            # region,state_province, postal_code,
+            '', r['state_province'], '',
+            # country
+            r['country']]
+                for r in parsed_data.addresses]
+        import_addresses(open_referral_csv_data, collector, counters)
+        result = Address.objects.all()
+        self.assertEqual(len(result), 2)
+
+        if result[0].city == mailing_city:
+            mailing_address = result[0]
+            physical_address = result[1]
+        else:
+            physical_address = result[0]
+            mailing_address = result[1]
+
+        self.assertEqual(physical_address.address[0:20], physical_address_1[0:20])
+        self.assertEqual(physical_address.city, physical_city)
+        self.assertEqual(mailing_address.address[0:20], mailing_address_1[0:20])
+        self.assertEqual(mailing_address.city, mailing_city)
 
     def test_service_with_taxonomy_term(self):
         # all upper case taxonomy terms are by convention assigned the bc211-what taxonomy
@@ -23,7 +164,7 @@ class TaxonomyEndToEndTest(TestCase):
 
         # create and parse the bc211 csv data for a service with a taxonomy term
 
-        data = (Bc211CsvDataBuilder().
+        bc211_csv_data = (Bc211CsvDataBuilder().
                 as_organization().
                 with_field('ResourceAgencyNum', the_organization_id).
                 with_field('PublicName', a_string()).
@@ -34,7 +175,7 @@ class TaxonomyEndToEndTest(TestCase):
                 with_field('ParentAgencyNum', the_organization_id).
                 with_field('TaxonomyTerm', bc211_what_taxonomy_term).
                 build())
-        parsed_data = parse(TestDataSink(), data)
+        parsed_data = parse(TestDataSink(), bc211_csv_data)
 
         self.assertEqual(len(parsed_data.organizations), 1)
         self.assertEqual(parsed_data.organizations[0]['id'], the_organization_id)
@@ -54,7 +195,7 @@ class TaxonomyEndToEndTest(TestCase):
 
         # import organization
 
-        data = [[
+        open_referral_csv_data = [[
             # id,name,alternate_name,
             r['id'], r['name'], r['alternate_name'],
             # description,email,url,
@@ -64,14 +205,14 @@ class TaxonomyEndToEndTest(TestCase):
                 for r in parsed_data.organizations]
         collector = InactiveRecordsCollector()
         counters = ImportCounters()
-        import_organization(data, collector, counters)
+        import_organization(open_referral_csv_data, collector, counters)
         result = Organization.objects.all()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].id, the_organization_id)
 
         # import service
 
-        data = [[
+        open_referral_csv_data = [[
             # id,organization_id,program_id,
             r['id'], r['organization_id'], '',
             # name,alternate_name,description,
@@ -83,7 +224,7 @@ class TaxonomyEndToEndTest(TestCase):
             # fees,accreditations,licenses, taxonomy_ids,last_verified_on-x
             '', '', '', '', '']
                 for r in parsed_data.services]
-        import_services(data, collector, counters)
+        import_services(open_referral_csv_data, collector, counters)
         result = Service.objects.all()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].id, the_service_id)
@@ -91,13 +232,13 @@ class TaxonomyEndToEndTest(TestCase):
 
         # import taxonomy terms
 
-        data = [[
+        open_referral_csv_data = [[
             # id,name,parent_id,
             r['id'], r['name'], '',
             # parent_name,vocabulary
             '', r['vocabulary']]
                 for r in parsed_data.taxonomy_terms]
-        import_taxonomy_terms(data, counters)
+        import_taxonomy_terms(open_referral_csv_data, counters)
         result = TaxonomyTerm.objects.all()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].taxonomy_term_id, taxonomy_term_id)
@@ -106,16 +247,13 @@ class TaxonomyEndToEndTest(TestCase):
 
         # import service taxonomy terms
 
-        data = [[
+        open_referral_csv_data = [[
             # id,service_id,taxonomy_id, taxonomy_detail
             r['id'], r['service_id'], r['taxonomy_id'], '']
                 for r in parsed_data.services_taxonomy]
-        import_service_taxonomy(data, collector)
+        import_service_taxonomy(open_referral_csv_data, collector)
         result = Service.objects.all()[0].taxonomy_terms.all()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].taxonomy_term_id, taxonomy_term_id)
         self.assertEqual(result[0].taxonomy_id, 'bc211-what')
         self.assertEqual(result[0].name, bc211_what_taxonomy_term.lower())
-
-
-# WARNING service_taxonomy TaxonomyTerm matching query does not exist.
