bb70c9d8845a06426616330aa670f665801b502d
Tomy Reyes <tomy@peacegeeks.org>
2021-01-22 10:13:41 -0800

    Issue #599 adjust prepare deploy script

--- utility/prepare_deploy.sh
+++ utility/prepare_deploy.sh
@@ -154,6 +154,33 @@ echo "importing BC-211 open referral csv data into the database..."
 ./manage.py import_open_referral_csv ./open_referral_csv_files --cityLatLongs $CityLatLongs
 checkForSuccess "import Bc-211 open referral data into the database"
 
+echo "converting organizationAsService CSV to open referral standard..."
+mkdir -p ./open_referral_csv_files_org_services
+./manage.py convert_icarol_csv ../content/organizationAsServices.csv ./open_referral_csv_files_org_services
+checkForSuccess "convert organizationAsService data into open referral standard"
+
+echo "importing organizationAsService open referral csv data into the database..."
+./manage.py import_open_referral_csv ./open_referral_csv_files_org_services --cityLatLongs $CityLatLongs
+checkForSuccess "import organizationAsService open referral data into the database"
+
+echo "converting additional schools CSV to open referral standard..."
+mkdir -p ./open_referral_csv_files_schools
+./manage.py convert_icarol_csv ../content/additionalSchools.csv ./open_referral_csv_files_schools
+checkForSuccess "convert additional schools data into open referral standard"
+
+echo "importing additional schools open referral csv data into the database..."
+./manage.py import_open_referral_csv ./open_referral_csv_files_schools --cityLatLongs $CityLatLongs
+checkForSuccess "import additional schools open referral data into the database"
+
+echo "converting additional libraries CSV to open referral standard..."
+mkdir -p ./open_referral_csv_files_libraries
+./manage.py convert_icarol_csv ../content/additionalLibraries.csv ./open_referral_csv_files_libraries
+checkForSuccess "convert additional libraries data into open referral standard"
+
+echo "importing additional libraries open referral csv data into the database..."
+./manage.py import_open_referral_csv ./open_referral_csv_files_libraries --cityLatLongs $CityLatLongs
+checkForSuccess "import additional libraries open referral data into the database"
+
 ./manage.py import_newcomers_guide $NewcomersGuidePath
 checkForSuccess "import newcomers guide data into the database"
 
