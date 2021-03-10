61f1e773681bc6676935e89920a118430a1092cb
rasmus <rasmus@peacegeeks.org>
2021-03-10 14:12:43 -0800

    Issue #611 ReorganizeFiles

---
+++ newcomers_guide/split_win_file.py
@@ -0,0 +1,69 @@
+import re
+
+def is_title(line):
+    regex = r'[\d\. ]*Topic:.*'
+    if re.fullmatch(regex, line):
+        return True
+    return False
+
+
+def get_title(line):
+    regex = r'[\d\. ]*Topic:(.*)'
+    result = re.match(regex, line)[1].strip()
+    if result == '':
+        raise RuntimeError(line)
+    return result
+
+
+def is_tag(line):
+    regex = r'Tags:.*'
+    if re.fullmatch(regex, line):
+        return True
+    return False
+
+
+def get_tags(line):
+    regex = r'Tags:(.*)'
+    result = re.match(regex, line)[1].strip().split()
+    return result
+
+
+class Topic:
+    def __init__(self, name, tags, text):
+        self.name = name
+        self.tags = tags
+        self.text = text
+
+
+class TopicWriter:
+    def __init__(self):
+        self.topics = []
+        self.name = ''
+        self.tags = []
+        self.text = []
+
+    def parse(self, line):
+        if is_title(line):
+            self.save_current_topic()
+            self.name = get_title(line)
+            self.tags = []
+            self.text = ''
+        elif is_tag(line):
+            self.tags = get_tags(line)
+        else:
+            self.text += line
+
+    def save_current_topic(self):
+        if self.name:
+            self.topics.append(Topic(self.name, self.tags, self.text))
+
+    def done(self):
+        self.save_current_topic()
+        return self
+
+
+def parse_string(text):
+    writer = TopicWriter()
+    for line in text.split('\n'):
+        writer.parse(line)
+    return writer.done()
